aae980d1c4b5491d5a31731cda8898ad1d2e32aa
imda-kwokwk <kwok_weng_kong@imda.gov.sg>
2023-06-07 19:25:25 +0800

    fix(setup-scripts): fix minor issues in setup scripts.
    - change folder name from deploy-scripts to setup-aiverify
    - correct typos in setup-aiverify-dev.sh
    - add mkdir's in docker-start.sh if required dir's do not exist, to ensure proper dir permissions
    - add mkdir for ~/logs/test-engine to resolve missing logs at report gen
    - add TEST_ENGINE_URL to portal .env.local in Dockerfile

--- setup-aiverify/aiverify-user/docker-start.sh
+++ setup-aiverify/aiverify-user/docker-start.sh
@@ -20,16 +20,19 @@ do
   i=$((i-1))
 done
 
-
 if [ "$RESET" == "--reset" ]; then
   echo "Cleaning up volumes"
   sudo rm -rf ~/data
   sudo rm -rf ~/uploads
-  mkdir -p ~/data/db
-  mkdir -p ~/uploads/data
-  mkdir -p ~/uploads/model
+  sudo rm -rf ~/logs
   docker-compose down --volumes
 fi
+
+[ ! -d "~/data/db" ] && mkdir -p ~/data/db
+[ ! -d "~/uploads/data" ] && mkdir -p ~/uploads/data
+[ ! -d "~/uploads/model" ] && mkdir -p ~/uploads/model
+[ ! -d "~/logs/db" ] && mkdir -p ~/logs/db
+[ ! -d "~/logs/test-engine" ] && mkdir -p ~/logs/test-engine
 
 echo "Starting containers..."
 docker-compose up $DETACH
