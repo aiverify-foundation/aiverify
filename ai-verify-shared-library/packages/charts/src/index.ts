dcf0414901cd21fdaa6694feac90d4d5b3960240
Peck Yoke <94153062+imda-peckyoke@users.noreply.github.com>
2023-06-12 19:13:56 +0800

    add radar chart and treemap (#73)

--- ai-verify-shared-library/packages/charts/src/index.ts
+++ ai-verify-shared-library/packages/charts/src/index.ts
@@ -3,3 +3,5 @@ export { default as LineChart, LineChartProps } from './lineChart';
 export { default as AreaChart, AreaChartProps } from './areaChart';
 export { default as PieChart, PieChartProps, renderCustomizedPieLabel } from './pieChart';
 export { default as ScatterChart, ScatterChartProps } from './scatterChart';
+export { default as Treemap, TreemapProps } from './treemap';
+export { default as RadarChart, RadarChartProps } from './radarChart';
