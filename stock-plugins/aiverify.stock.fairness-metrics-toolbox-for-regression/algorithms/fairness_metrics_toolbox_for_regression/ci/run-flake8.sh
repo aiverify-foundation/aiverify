750ff0b6f3a9d52dbad8608924e813cd03e60593
imda-kwokwk <kwok_weng_kong@imda.gov.sg>
2023-06-30 10:03:54 +0800

    fix(pre-merge check): fix ci scripts.
    - include -i option for coverage html and coverage json commands in run-test.sh to ignore irrelevant errors
    - make run-flake8.sh, run-pip-audit.sh more verbose
    - remove ci folder from image-corruption-toolbox/algorithms folder as it was treated as an algo folder by run-test.sh

--- stock-plugins/aiverify.stock.fairness-metrics-toolbox-for-regression/algorithms/fairness_metrics_toolbox_for_regression/ci/run-flake8.sh
+++ stock-plugins/aiverify.stock.fairness-metrics-toolbox-for-regression/algorithms/fairness_metrics_toolbox_for_regression/ci/run-flake8.sh
@@ -4,6 +4,8 @@ source_dir=.
 set +e
 flake8 --format=html --htmldir=flake8-report --count  $source_dir > flake8-report.txt
 exit_code=$?
+flake8 $source_dir
+cat flake8-report.txt
 python3 ci/createBadges.py lint
 
 if [ $exit_code -ne 0 ]; then
