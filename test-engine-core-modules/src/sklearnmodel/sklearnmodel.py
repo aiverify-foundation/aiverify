706dff10e94ee48d3234072e171500673cc2bbb7
Kelvin Kok <kelvin_kok@imda.gov.sg>
2023-08-01 13:22:06 +0800

    added sklearn Extra Tree Regressor to list of supported models

--- test-engine-core-modules/src/sklearnmodel/sklearnmodel.py
+++ test-engine-core-modules/src/sklearnmodel/sklearnmodel.py
@@ -29,6 +29,7 @@ class Plugin(IModel):
         "sklearn.ensemble._bagging.BaggingClassifier",
         "sklearn.linear_model._base.LinearRegression",
         "sklearn.ensemble._gb.GradientBoostingRegressor",
+        "sklearn.ensemble.ExtraTreesRegressor",
     ]
     _name: str = "sklearnmodel"
     _description: str = "sklearnmodel supports detecting sklearn models"
