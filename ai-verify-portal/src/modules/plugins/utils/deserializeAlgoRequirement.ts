cd1ed38e2c1c7c2a106d7f835d503624e6aa340e
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2023-08-16 14:54:56 +0800

    bugfix - set width to widget description text to prevent overlap, and fix plugins search regex

--- ai-verify-portal/src/modules/plugins/utils/deserializeAlgoRequirement.ts
+++ ai-verify-portal/src/modules/plugins/utils/deserializeAlgoRequirement.ts
@@ -4,7 +4,7 @@ import { AlgoDependency } from '../pluginDetails';
 export function deserializeAlgoRequirement(
   algoReq: string
 ): AlgoDependency | undefined {
-  const regx = /([a-zA-z0-9-_.@]*)([=><]{1,2})(\d*.\d*.\d*)/;
+  const regx = /([a-zA-Z0-9-_.@]*)([=><]{1,2})(\d*.\d*.\d*)/;
   const matches = algoReq.trim().match(regx);
   if (matches && matches.length > 0) {
     return [matches[1], matches[2], matches[3]];
