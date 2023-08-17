cbbaf6fa2981e4bd754733bd895db802649b0b4c
Amin <Aminurrashid_MOHAMED_LAHIR@imda.gov.sg>
2023-08-17 10:58:05 +0800

    update unit tests - assert datasets/model screen is rendered

--- ai-verify-portal/__tests__/modules/project.test.tsx
+++ ai-verify-portal/__tests__/modules/project.test.tsx
@@ -252,66 +252,254 @@ describe('Project Flow', () => {
         y: 218,
       });
 
+      //widget should be on canvas
       await waitFor(() => {
         expect(container.querySelector('.widgetContainer')).not.toBeNull();
       });
-
       expect(gridLayoutArea.children.length).toBe(1);
-      expect(container.querySelector('.widgetContainer'))
-        .toMatchInlineSnapshot(`
-          <div
-            class="widgetContainer"
-          >
-            <div
-              style="width: 100%;"
-            >
-              <p>
-                <b>
-                  Results
-                </b>
-              </p>
-              <p>
-                The difference between 
-                <b>
-                  <i>
-                    gender:0,age:21
-                  </i>
-                </b>
-                 (group with highest value) and 
-                <b>
-                  <i>
-                    gender:0,age:18
-                  </i>
-                </b>
-                 (group with lowest value) is 
-                <b>
-                  2607.2669363473
-                </b>
-                 for 
-                <i>
-                  Mean Absolute Error Parity
-                </i>
-                . In an ideal situation, the parity should be close to 
-                0
-                .
-              </p>
-            </div>
-          </div>
-        `);
+      //snapshot of widget
+      expect(container.querySelector('.widgetContainer')).toMatchSnapshot(
+        'widget-on-canvas'
+      );
 
+      //widget is highlighted. It's name tag and delete button should be available
       expect(container.querySelector('.menuContainer')).not.toBeNull();
       expect(
         (container.querySelector('.gridItem_title') as HTMLElement).textContent
       ).toBe('Fairness for Regression - Interpretation (MAE)');
       expect(container.querySelector('.gridItem_btnWrapper')).not.toBeNull();
       expect(container.querySelector('.canvas_item_highlight')).not.toBeNull();
-      await user.click(
-        await screen.findByPlaceholderText(/^Search Report Widgets$/i)
-      );
+
+      //Snapshot widget properties panel
+      //NaN x & y in snapshot is fine. We are rendering in memory. Have not figured out how to mock the x/y positions.
+      expect(
+        container.querySelector('#aivWidgetPropertiesPanel')
+      ).toMatchSnapshot('widget-properties-panel');
+
+      //color picker 1
+      await user.click(container.querySelectorAll('.colorBox')[0]);
+      expect(container.querySelector('.aiv-colorpicker')).not.toBeNull();
+      await user.click(container);
+      expect(container.querySelector('.aiv-colorpicker')).toBeNull();
+
+      //color picker 2
+      await user.click(container.querySelectorAll('.colorBox')[1]);
+      expect(container.querySelector('.aiv-colorpicker')).not.toBeNull();
+      await user.click(container);
+      expect(container.querySelector('.aiv-colorpicker')).toBeNull();
+
+      //widget highlight, name tag and delete button should disappear when outside clicked
+      await user.click(container);
       expect(container.querySelector('.menuContainer')).toBeNull();
       expect(container.querySelector('.gridItem_title')).toBeNull();
       expect(container.querySelector('.gridItem_btnWrapper')).toBeNull();
       expect(container.querySelector('.canvas_item_highlight')).toBeNull();
-    }, 20000);
+
+      //test to run
+      expect(
+        (container.querySelector('.testsCountChip') as HTMLElement).textContent
+      ).toBe('1');
+      expect(
+        (
+          await screen.findAllByText(
+            /^Fairness Metrics Toolbox for Regression$/i
+          )
+        ).length
+      ).toBe(1);
+    });
+
+    it('should toggle canvas grid and add / delete pages', async () => {
+      const fetchSpy = jest.spyOn(global, 'fetch');
+      fetchSpy.mockImplementation(
+        jest.fn(() =>
+          Promise.resolve({
+            json: () => Promise.resolve(fmtInterpretationMAEBundleResponse),
+          })
+        ) as jest.Mock
+      );
+      const user = userEvent.setup();
+      const { container } = render(
+        <MockProviders apolloMocks={mockGqlDataMinimal}>
+          <ProjectCreatePageWrapper />
+        </MockProviders>
+      );
+      const projNameInput: HTMLInputElement =
+        await screen.findByPlaceholderText(
+          /^Enter name of this project e.g. Credit Scoring Model Tests$/i
+        );
+      await userEvent.type(projNameInput, 'Test Project');
+      const nextBtn = await screen.findByText(/^Next$/i);
+      await user.click(nextBtn.parentElement as HTMLDivElement);
+      await screen.findByText(/^Select Report Template$/i);
+      const blankCard = await screen.findByText(/^Blank Canvas$/i);
+      await userEvent.click(blankCard);
+      await waitFor(() => {
+        expect(container.querySelector('.card__highlighted')).toBeDefined();
+      });
+      await userEvent.click(nextBtn);
+      await screen.queryByText(/^Design Report$/i);
+      await screen.findByText(/^Interpretation \(MAE\)$/i);
+      const gridLayoutArea = container.querySelector(
+        GRID_LAYOUT_CLASSNAME
+      ) as HTMLDivElement;
+      expect(gridLayoutArea.children.length).toBe(0);
+      const maeInterWidget = container.querySelector(
+        '[data-test-id="draggableWidget-aiverify.stock.fairness_metrics_toolbox_for_regression:mae_interpretation"]'
+      );
+      fireEvent.dragStart(maeInterWidget as Element, {
+        dataTransfer: { setData: (_str: string) => undefined },
+        clientX: 518,
+        clientY: 326,
+        screenX: -1402,
+        screenY: 435,
+        x: 374,
+        y: 218,
+      });
+
+      fireEvent.dragOver(gridLayoutArea as Element, {
+        clientX: 518,
+        clientY: 326,
+        screenX: -1402,
+        screenY: 435,
+        x: 374,
+        y: 218,
+      });
+
+      await waitFor(() => {
+        expect(container.querySelector(GRID_ITEM_CLASSNAME)).not.toBeNull();
+      });
+
+      fireEvent.drop(gridLayoutArea as Element, {
+        clientX: 518,
+        clientY: 326,
+        screenX: -1402,
+        screenY: 435,
+        x: 374,
+        y: 218,
+      });
+
+      //widget should be on canvas
+      await waitFor(() => {
+        expect(container.querySelector('.widgetContainer')).not.toBeNull();
+      });
+
+      const topMenuIcons = container.querySelectorAll(
+        '.canvas_topbar .iconBtn'
+      );
+      expect(topMenuIcons.length).toBe(4);
+
+      //grid toggle
+      expect(container.querySelector('.canvas_grid')).not.toBeNull();
+      await user.click(topMenuIcons[0]);
+      expect(container.querySelector('.canvas_grid')).toBeNull();
+
+      //multiple pages
+      expect(container.querySelectorAll('.MuiPaginationItem-page').length).toBe(
+        1
+      );
+      await user.click(topMenuIcons[2]);
+      const addPageElems = await screen.findAllByText(/^Add Page$/i);
+      await user.click(addPageElems[1]);
+      const pageNumbers = container.querySelectorAll('.MuiPaginationItem-page');
+      expect(pageNumbers.length).toBe(2);
+      expect(gridLayoutArea.children.length).toBe(0); //new page has no widget
+      await user.click(pageNumbers[0]);
+      expect(gridLayoutArea.children.length).toBe(1); //back on page 1 which has 1 widget
+
+      //delete page 1
+      await user.click(topMenuIcons[1]);
+      await screen.findByText(/^Delete Page$/i);
+      await user.click(await screen.findByText(/^Proceed$/i));
+      expect(container.querySelectorAll('.MuiPaginationItem-page').length).toBe(
+        1
+      );
+      expect(gridLayoutArea.children.length).toBe(0); //remaining page has no widget
+    });
+  });
+
+  describe('Datasets and Model Selection Screen', () => {
+    it('should render Datasets / Model selection screen', async () => {
+      const fetchSpy = jest.spyOn(global, 'fetch');
+      fetchSpy.mockImplementation(
+        jest.fn(() =>
+          Promise.resolve({
+            json: () => Promise.resolve(fmtInterpretationMAEBundleResponse),
+          })
+        ) as jest.Mock
+      );
+      const user = userEvent.setup();
+      const { container } = render(
+        <MockProviders apolloMocks={mockGqlDataMinimal}>
+          <ProjectCreatePageWrapper />
+        </MockProviders>
+      );
+      const projNameInput: HTMLInputElement =
+        await screen.findByPlaceholderText(
+          /^Enter name of this project e.g. Credit Scoring Model Tests$/i
+        );
+      await userEvent.type(projNameInput, 'Test Project');
+      const nextBtn = await screen.findByText(/^Next$/i);
+      await user.click(nextBtn.parentElement as HTMLDivElement);
+      await screen.findByText(/^Select Report Template$/i);
+      const blankCard = await screen.findByText(/^Blank Canvas$/i);
+      await userEvent.click(blankCard);
+      await waitFor(() => {
+        expect(container.querySelector('.card__highlighted')).toBeDefined();
+      });
+      await userEvent.click(nextBtn);
+      await screen.queryByText(/^Design Report$/i);
+      await screen.findByText(/^Interpretation \(MAE\)$/i);
+      const gridLayoutArea = container.querySelector(
+        GRID_LAYOUT_CLASSNAME
+      ) as HTMLDivElement;
+      expect(gridLayoutArea.children.length).toBe(0);
+      const maeInterWidget = container.querySelector(
+        '[data-test-id="draggableWidget-aiverify.stock.fairness_metrics_toolbox_for_regression:mae_interpretation"]'
+      );
+      fireEvent.dragStart(maeInterWidget as Element, {
+        dataTransfer: { setData: (_str: string) => undefined },
+        clientX: 518,
+        clientY: 326,
+        screenX: -1402,
+        screenY: 435,
+        x: 374,
+        y: 218,
+      });
+
+      fireEvent.dragOver(gridLayoutArea as Element, {
+        clientX: 518,
+        clientY: 326,
+        screenX: -1402,
+        screenY: 435,
+        x: 374,
+        y: 218,
+      });
+
+      await waitFor(() => {
+        expect(container.querySelector(GRID_ITEM_CLASSNAME)).not.toBeNull();
+      });
+
+      fireEvent.drop(gridLayoutArea as Element, {
+        clientX: 518,
+        clientY: 326,
+        screenX: -1402,
+        screenY: 435,
+        x: 374,
+        y: 218,
+      });
+
+      //widget should be on canvas
+      await waitFor(() => {
+        expect(container.querySelector('.widgetContainer')).not.toBeNull();
+      });
+
+      await user.click(await screen.findByText(/^Next$/i));
+      await screen.findByText(
+        /^Select the Datasets and AI Model to be tested$/i
+      );
+      expect(container.querySelector('.layoutContentArea')).toMatchSnapshot(
+        'Datasets/Model-Selection-Screen'
+      ); // snapshot without header, because header has dynamic autosave time display
+    });
   });
 });
