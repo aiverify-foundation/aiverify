16da74cd7ff656b228ebe04682bd99ed956ee4f8
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-01-17 10:21:32 +0800

    remove env file exist check

--- ai-verify-apigw/lib/mongodb.mjs
+++ ai-verify-apigw/lib/mongodb.mjs
@@ -1,21 +1,14 @@
 'use strict'
 
 import mongoose from 'mongoose';
-import readline from 'readline';
-import fs from 'fs';
-
-if (fs.existsSync('.env') === false && fs.existsSync('.env.local') === false) {
-  console.log('\x1b[31m\x1b[43m%s\x1b[0m', 'ai-verify-apigw - .env file not found. Please create a .env file in the root of the project.');
-  process.exit(1);
-}
 
 if (!process.env.DB_USERNAME || !process.env.DB_PASSWORD) {
-  console.log('\x1b[31m\x1b[43m%s\x1b[0m', 'ai-verify-apigw - DB_USERNAME or DB_PASSWORD not found. Please add them to the .env file in the root of the project.');
+  console.log('\x1b[31m\x1b[43m%s\x1b[0m', 'ai-verify-apigw - DB_USERNAME or DB_PASSWORD not found. Please add them to the .env file in the root of the ai-verify-apigw project.');
   process.exit(1);
 }
 
 if (!process.env.DB_HOST || !process.env.DB_PORT) {
-  console.log('\x1b[31m\x1b[43m%s\x1b[0m', 'ai-verify-apigw - DB_HOST or DB_PORT not found. Please add them to the .env file in the root of the project.');
+  console.log('\x1b[31m\x1b[43m%s\x1b[0m', 'ai-verify-apigw - DB_HOST or DB_PORT not found. Please add them to the .env file in the root of ai-verify-apigw project.');
   process.exit(1);
 }
 
@@ -28,7 +21,7 @@ var db = mongoose.connection;
 db.on('error', err => {
   if (err.message.includes('Authentication failed')) {
     console.log('\x1b[31m\x1b[43m%s\x1b[0m', 'ai-verify-apigw - Authentication failed while connecting to DB. Please ensure the username and password has been set up in the database. Check your username and password are correct.');
-    console.log('\x1b[31m\x1b[43m%s\x1b[0m', 'ai-verify-apigw - If credentials are correct, please check that "aiverify" has been set up in the database.');
+    console.log('\x1b[31m\x1b[43m%s\x1b[0m', 'ai-verify-apigw - If credentials are correct, please check that the database named "aiverify" has been set up in mongodb');
   } else {
     console.error('An error occured while connecting to DB: %s', err.message);
   }
