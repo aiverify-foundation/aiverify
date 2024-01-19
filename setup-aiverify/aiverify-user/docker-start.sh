2df264e696a7ee86b319a22a0dac3c703d9cdeb5
imda-kwokwk <kwok_weng_kong@imda.gov.sg>
2024-01-19 14:57:25 +0800

    fix-docker-passwd - generate random username and passwords for mongo db instead of hardcoding in Dockerfile

--- setup-aiverify/aiverify-user/docker-start.sh
+++ setup-aiverify/aiverify-user/docker-start.sh
@@ -4,9 +4,9 @@
 export CUR_UID=$(id -u)
 export CUR_GID=$(id -g)
 
-export MONGO_ROOT_PASSWORD="48a93c85e92d9c107a356d583969466c"
-export DB_AIVERIFY_USER="3c82d11255cf9fab"
-export DB_AIVERIFY_PASSWORD="f352d9ca304fe2772e2fbe42b96985b9"
+export MONGO_ROOT_PASSWORD=
+export DB_AIVERIFY_USER=
+export DB_AIVERIFY_PASSWORD=
 
 [ ! -d "~/data/db" ] && mkdir -p ~/data/db
 [ ! -d "~/logs/db" ] && mkdir -p ~/logs/db
