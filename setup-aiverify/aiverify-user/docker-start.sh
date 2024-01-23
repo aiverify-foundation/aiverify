4fc3925b72a77c38130cc48ec279fa1127e3f95a
imda-kwokwk <kwok_weng_kong@imda.gov.sg>
2024-01-23 11:59:32 +0800

    fix-docker-passwd - prompt user for db username and password instead of hardcoding in config file in Dockerfile

--- setup-aiverify/aiverify-user/docker-start.sh
+++ setup-aiverify/aiverify-user/docker-start.sh
@@ -4,16 +4,16 @@
 export CUR_UID=$(id -u)
 export CUR_GID=$(id -g)
 
-# Prompt user for user id
-read -p "Enter user id: " DB_AIVERIFY_USERNAME
+# Prompt user for username
+read -p "Enter user name: " DB_AIVERIFY_USERNAME
 
 # Prompt user for password
 read -s -p "Enter password: " DB_AIVERIFY_PASSWORD
 echo
 
-# Check if user id and password are provided
+# Check if username and password are provided
 if [ -z "$DB_AIVERIFY_USERNAME" ] || [ -z "$DB_AIVERIFY_PASSWORD" ]; then
-    echo "User id and password are required. Aborting."
+    echo "User name and password are required. Aborting aiverify containers start-up..."
     exit 1
 fi
 
@@ -21,12 +21,8 @@ export DB_AIVERIFY_USER="$DB_AIVERIFY_USERNAME"
 export DB_AIVERIFY_PASSWORD="$DB_AIVERIFY_PASSWORD"
 export MONGO_ROOT_PASSWORD="$DB_AIVERIFY_PASSWORD"
 
-#export MONGO_ROOT_PASSWORD=
-#export DB_AIVERIFY_USER=
-#export DB_AIVERIFY_PASSWORD=
-
 [ ! -d "~/data/db" ] && mkdir -p ~/data/db
 [ ! -d "~/logs/db" ] && mkdir -p ~/logs/db
 
-echo "Starting aiverify containers..."
+echo "Starting up aiverify containers..."
 docker-compose up ${@:1}
