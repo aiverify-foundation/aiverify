96178a4faae410b61faf9bd2118d73bb41963932
Benedict Lee <benedict_lee@imda.gov.sg>
2024-01-24 02:23:28 +0800

    Clean up Commented Code

--- setup-aiverify/aiverify-user/docker-build.sh
+++ setup-aiverify/aiverify-user/docker-build.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
 
-# Generate random passwords
-#db_aiverify_user=$(openssl rand -hex 8)
-#db_aiverify_password=$(openssl rand -hex 16)
-#mongo_root_password=$(openssl rand -hex 16)
-#
-#echo "DB_AIVERIFY_USER: $db_aiverify_user"
-#echo "DB_AIVERIFY_PASSWORD: $db_aiverify_password"
-#echo "MONGO_ROOT_PASSWORD: $mongo_root_password"
-#
-#file_path="docker-start.sh"
-#sed -i '' "s/export DB_AIVERIFY_USERNAME.*/export DB_AIVERIFY_USERNAME\"$db_aiverify_user\"/" "$file_path"
-#sed -i '' "s/export DB_AIVERIFY_PASSWORD.*/export DB_AIVERIFY_PASSWORD=\"$db_aiverify_password\"/" "$file_path"
-#sed -i '' "s/export MONGO_ROOT_PASSWORD.*/export MONGO_ROOT_PASSWORD=\"$mongo_root_password\"/" "$file_path"
-#
-#docker build --build-arg MONGO_USERNAME=${db_aiverify_user} \
-#             --build-arg MONGO_PASSWORD=${db_aiverify_password} \
-#             ${@:1} -t aiverify:0.10 .
-
 docker build ${@:1} -t aiverify:0.10 .
-
-
-
