8d0412da95118f93580a94e258dd2d1b74b1f7a1
imda-kwokwk <kwok_weng_kong@imda.gov.sg>
2024-05-21 09:30:53 +0800

    Change IMDA-BTG references to aiverify-foundation in the GHA yml files to align with org name change.

--- setup-aiverify/aiverify-user/docker-pull.sh
+++ setup-aiverify/aiverify-user/docker-pull.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Pull aiverify image from Github Container Registry (ghcr) to local registry
-docker pull ghcr.io/imda-btg/aiverify:0.9
+docker pull ghcr.io/aiverify-foundation/aiverify:0.9
 if [[ ! $(docker images -q aiverify:0.9) = "" ]]; then
   docker rmi aiverify:0.9
 fi
-docker tag ghcr.io/imda-btg/aiverify:0.10 aiverify:0.10
-docker rmi ghcr.io/imda-btg/aiverify:0.10
+docker tag ghcr.io/aiverify-foundation/aiverify:0.10 aiverify:0.10
+docker rmi ghcr.io/aiverify-foundation/aiverify:0.10
