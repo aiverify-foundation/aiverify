2cc0f36da73112c91019fd6b5bec71c084f44ead
Peck Yoke <leong_peck_yoke@imda.gov.sg>
2024-08-28 23:12:13 +0800

    add check_plugin_registry as part of initilization

--- aiverify-apigw/aiverify_apigw/__main__.py
+++ aiverify-apigw/aiverify_apigw/__main__.py
@@ -1,7 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+host = os.getenv("APIGW_HOST_ADDRESS", "127.0.0.1")
+port = int(os.getenv("APIGW_PORT", 4000))
+
 import uvicorn
 from fastapi import FastAPI
 from .routers import test_result_router
 from .lib.database import init_db
+from .lib.plugin_store import PluginStore
 
 app = FastAPI(
     title="AI Verify API Gateway",
@@ -13,15 +21,8 @@ app = FastAPI(
 app.include_router(test_result_router.router)
 
 if __name__ == "__main__":
-    import os
-    from dotenv import load_dotenv
-
-    load_dotenv()
-
-    host = os.getenv("APIGW_HOST_ADDRESS", "127.0.0.1")
-    port = int(os.getenv("APIGW_PORT", 4000))
-
     # init db
     init_db()
+    PluginStore.check_plugin_registry()
 
     uvicorn.run(app, host=host, port=port)
