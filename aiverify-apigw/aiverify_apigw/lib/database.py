a4b01b3416c31e87006198d55dbb4ed09a3819c2
Peck Yoke <leong_peck_yoke@imda.gov.sg>
2024-09-07 00:07:36 +0800

    add method get_db_session for dependency injection

--- aiverify-apigw/aiverify_apigw/lib/database.py
+++ aiverify-apigw/aiverify_apigw/lib/database.py
@@ -6,11 +6,19 @@ from ..models.base_model import BaseORMModel
 def init_db():
     import os
     db_uri = os.getenv("APIGW_DB_URI", "sqlite://")
-    engine = create_engine(db_uri, echo=True)
-    BaseORMModel.metadata.create_all(engine)
+    engine = create_engine(db_uri, echo=True, connect_args={"check_same_thread": False})
     return engine
 
 
 engine = init_db()
 
 SessionLocal = sessionmaker(engine)
+
+
+def get_db_session():
+    """Dependency to provide a database session."""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
