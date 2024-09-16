558d77645aa907f8d23d0fc81e471ec08cb9cf98
Suresh Kumar Kushal Raj <sureshjain@sureshs-MacBook-Pro.local>
2024-09-16 13:25:22 +0800

    updated e2e tests

--- stock-plugins/aiverify.stock.fairness-metrics-toolbox-for-regression/algorithms/fairness_metrics_toolbox_for_regression/tests/e2e/test_e2e.py
+++ stock-plugins/aiverify.stock.fairness-metrics-toolbox-for-regression/algorithms/fairness_metrics_toolbox_for_regression/tests/e2e/test_e2e.py
@@ -20,9 +20,7 @@ regression_non_pipeline = {
 
 binary_pipeline = {
     "data_path": str("../../../user_defined_files/data/sample_reg_pipeline_data.sav"),
-    "model_path": str(
-        "../../../user_defined_files/data/sample_reg_pipeline_ytest_data.sav"
-    ),
+    "model_path": str("../../../user_defined_files/pipeline"),
     "ground_truth_path": str(
         "../../../user_defined_files/data/sample_reg_pipeline_ytest_data.sav"
     ),
@@ -38,27 +36,23 @@ binary_pipeline = {
     [regression_non_pipeline, binary_pipeline],
 )
 def test_fairness_metrics_toolbox_for_regression_plugin(data_set):
-    try:
-        # Create an instance of PluginTest with defined paths and arguments and Run.
-        core_modules_path = ""
-        plugin_argument_values = {
-            "sensitive_feature": data_set.sensitive_feature,
-        }
+    # Create an instance of PluginTest with defined paths and arguments and Run.
+    core_modules_path = ""
+    plugin_argument_values = {
+        "sensitive_feature": data_set["sensitive_feature"],
+    }
 
-        plugin_test = AlgoInit(
-            data_set["run_pipeline"],
-            core_modules_path,
-            data_set["data_path"],
-            data_set["model_path"],
-            data_set["ground_truth_path"],
-            data_set["ground_truth"],
-            data_set["model_type"],
-            plugin_argument_values,
-        )
-        plugin_test.run()
+    plugin_test = AlgoInit(
+        data_set["run_pipeline"],
+        core_modules_path,
+        data_set["data_path"],
+        data_set["model_path"],
+        data_set["ground_truth_path"],
+        data_set["ground_truth"],
+        data_set["model_type"],
+        plugin_argument_values,
+    )
+    plugin_test.run()
 
-        json_file_path = Path.cwd() / "output" / "results.json"
-        assert json_file_path.exists(), f"File not found: {json_file_path}"
-
-    except Exception as exception:
-        print(f"Exception caught while running the plugin test: {str(exception)}")
+    json_file_path = Path.cwd() / "output" / "results.json"
+    assert json_file_path.exists()
