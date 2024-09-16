ac496d15f86b6bcc28eff352f1a20a4112a42c28
Suresh Kumar Kushal Raj <sureshjain@sureshs-MacBook-Pro.local>
2024-09-16 13:28:24 +0800

    updating e2e tests

--- stock-plugins/aiverify.stock.accumulated-local-effect/algorithms/accumulated_local_effect/tests/e2e/test_e2e.py
+++ stock-plugins/aiverify.stock.accumulated-local-effect/algorithms/accumulated_local_effect/tests/e2e/test_e2e.py
@@ -61,32 +61,23 @@ multiclass_pipeline = {
     "data_set",
     [binary_classification, multiclass, tabular_binary_pipeline, multiclass_pipeline],
 )
-def test_ALE_plugin(data_set, caplog):
+def test_ALE_plugin(data_set):
     core_modules_path = ""
     # =====================================================================================
     # NOTE: Do not modify the code below
     # =====================================================================================
     # Perform Plugin Testing
-    try:
-        # Create an instance of PluginInit with defined paths and arguments and Run.
-        # with caplog.at_level(logging.INFO):
-        # assert 1==2
-        plugin_test = AlgoInit(
-            data_set["run_pipeline"],
-            core_modules_path,
-            data_set["data_path"],
-            data_set["model_path"],
-            data_set["ground_truth_path"],
-            data_set["ground_truth"],
-            data_set["model_type"],
-        )
-        plugin_test.run()
 
-        json_file_path = Path.cwd() / "output" / "results.json"
-        assert json_file_path.exists(), f"File not found: {json_file_path}"
+    plugin_test = AlgoInit(
+        data_set["run_pipeline"],
+        core_modules_path,
+        data_set["data_path"],
+        data_set["model_path"],
+        data_set["ground_truth_path"],
+        data_set["ground_truth"],
+        data_set["model_type"],
+    )
+    plugin_test.run()
 
-        print("Assertions passed, continuing execution...")
-        # assert "This is a test log message" in caplog.text
-    except Exception as e:
-        # Handle and report any exceptions that occur during plugin execution
-        print(f"Exception caught while running the plugin test: {str(e)}")
+    json_file_path = Path.cwd() / "output" / "results.json"
+    assert json_file_path.exists()
