af7f7c933393b614dce57c24fc3c696b94a427cf
Peck Yoke <leong_peck_yoke@imda.gov.sg>
2024-09-17 21:52:40 +0800

    change model_type to non-optional

--- aiverify-apigw/aiverify_apigw/models/test_model_model.py
+++ aiverify-apigw/aiverify_apigw/models/test_model_model.py
@@ -17,7 +17,7 @@ class TestModelModel(BaseORMModel):
         Enum(TestModelMode), nullable=False)
     file_type: Mapped[Optional[TestModelFileType]] = mapped_column(
         Enum(TestModelFileType))  # only if mode == upload
-    model_type: Mapped[Optional[ModelType]] = mapped_column(Enum(ModelType), nullable=True)
+    model_type: Mapped[ModelType] = mapped_column(Enum(ModelType), nullable=False)
     status: Mapped[TestModelStatus] = mapped_column(
         Enum(TestModelStatus), nullable=False)
     size: Mapped[Optional[float]]
