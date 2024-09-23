f51a13cd42dcdde0986be458878fb1e3b0a6e9b3
Suresh Kumar Kushal Raj <sureshjain@sureshs-MacBook-Pro.local>
2024-09-23 19:34:43 +0800

    refactor shap-ttolbar and add support for remote plugin

--- stock-plugins/aiverify.stock.shap-toolbox/algorithms/shap_toolbox/tests/unit_tests/test_algo.py
+++ stock-plugins/aiverify.stock.shap-toolbox/algorithms/shap_toolbox/tests/unit_tests/test_algo.py
@@ -1,9 +1,10 @@
+import importlib
 import json
 import logging
 from pathlib import Path
 
 import pytest
-from shap_toolbox import Plugin
+from aiverify_shap_toolbox.algo import Plugin
 from test_engine_core.interfaces.idata import IData
 from test_engine_core.interfaces.imodel import IModel
 from test_engine_core.plugins.enums.model_type import ModelType
@@ -16,14 +17,18 @@ from test_engine_core.utils.simple_progress import SimpleProgress
 
 def test_discover_plugin():
     PluginManager.discover(
-        str(Path().absolute() / "../../../../test-engine-core-modules")
+        str(Path(importlib.util.find_spec("test_engine_core").origin).parent.resolve())
     )
 
 
 # Variables for testing
-valid_data_path = "tests/user_defined_files/data/sample_bc_credit_data.sav"
-valid_model_path = "tests/user_defined_files/model/sample_bc_credit_sklearn_linear.LogisticRegression.sav"
-valid_ground_truth_path = "tests/user_defined_files/data/sample_bc_credit_data.sav"
+valid_data_path = str("../../../user_defined_files/data/sample_bc_credit_data.sav")
+valid_model_path = str(
+    "../../../user_defined_files/model/sample_bc_credit_sklearn_linear.LogisticRegression.sav"
+)
+valid_ground_truth_path = str(
+    "../../../user_defined_files//data/sample_bc_credit_data.sav"
+)
 
 test_string = "data_str"
 test_int = 1
@@ -35,7 +40,7 @@ test_tuple = ("data_str", "data_str")
 test_none = None
 
 
-class TestObject:
+class ObjectTest:
     def __init__(self):
         test_discover_plugin()
         (
@@ -65,7 +70,7 @@ class TestObject:
         input_args = {
             "explain_type": "global",
             "background_path": (
-                "tests/user_defined_files/data/" "sample_bc_credit_data.sav"
+                "../../../user_defined_files/data/" "sample_bc_credit_data.sav"
             ),
             "background_samples": 25,
             "data_samples": 25,
@@ -124,7 +129,7 @@ def get_data_instance_and_serializer_without_ground_truth(request):
         data_serializer_instance,
         data_error_message,
     ) = PluginManager.get_instance(PluginType.DATA, **{"filename": request.param})
-    test_object = TestObject()
+    test_object = ObjectTest()
     data_instance.remove_ground_truth(test_object._ground_truth)
     yield (data_instance, data_serializer_instance)
 
@@ -176,7 +181,7 @@ def get_ground_truth_instance_and_serializer(request):
 
 
 def test_create_plugin_instance_with_all_valid_input():
-    test_object = TestObject()
+    test_object = ObjectTest()
     test_plugin = Plugin(
         test_object._data_instance_and_serializer,
         test_object._model_instance_and_serializer,
@@ -200,7 +205,7 @@ def test_create_plugin_instance_with_all_valid_input():
 def test_init_plugin_instance_with_invalid_data_instance_type(
     invalid_data_instance_type,
 ):
-    test_object = TestObject()
+    test_object = ObjectTest()
     expected_exception_msg = "The algorithm has failed data validation"
     with pytest.raises(test_object._expected_exception) as excinfo:
         Plugin(
@@ -222,7 +227,7 @@ def test_init_plugin_instance_with_invalid_data_instance_type(
 def test_init_plugin_instance_with_invalid_model_instance_type(
     invalid_model_instance_type,
 ):
-    test_object = TestObject()
+    test_object = ObjectTest()
     expected_exception_msg = "The algorithm has failed model validation"
     with pytest.raises(test_object._expected_exception) as excinfo:
         Plugin(
@@ -247,7 +252,7 @@ def test_init_plugin_instance_with_invalid_model_instance_type(
 def test_init_plugin_instance_with_invalid_ground_truth_instance_type(
     invalid_ground_truth_instance_type,
 ):
-    test_object = TestObject()
+    test_object = ObjectTest()
     expected_exception_msg = "The algorithm has failed ground truth data validation"
     with pytest.raises(test_object._expected_exception) as excinfo:
         Plugin(
@@ -269,7 +274,7 @@ def test_init_plugin_instance_with_invalid_ground_truth_instance_type(
     "ground_truth", [test_int, test_float, test_list, test_dict, test_tuple]
 )
 def test_init_plugin_instance_with_invalid_ground_truth_type(ground_truth):
-    test_object = TestObject()
+    test_object = ObjectTest()
     test_object._input_args["ground_truth"] = ground_truth
     expected_exception_msg = "The algorithm has failed ground truth header validation."
     with pytest.raises(test_object._expected_exception) as excinfo:
@@ -287,7 +292,7 @@ def test_init_plugin_instance_with_invalid_ground_truth_type(ground_truth):
 
 @pytest.mark.parametrize("ground_truth", [test_none])
 def test_init_plugin_instance_with_none_ground_truth_type(ground_truth):
-    test_object = TestObject()
+    test_object = ObjectTest()
     test_object._input_args["ground_truth"] = ground_truth
     expected_exception_msg = "The algorithm has failed ground truth header validation."
     with pytest.raises(test_object._expected_exception) as excinfo:
@@ -357,7 +362,7 @@ def test_init_plugin_instance_with_missing_ground_truth(
     ],
 )
 def test_setup_plugin_instance_with_invalid_model_type(invalid_model_type):
-    test_object = TestObject()
+    test_object = ObjectTest()
     test_object._input_args["model_type"] = invalid_model_type
     expected_exception_msg = "The algorithm has failed validation for model type"
     with pytest.raises(test_object._expected_exception) as excinfo:
@@ -378,7 +383,7 @@ def test_setup_plugin_instance_with_invalid_model_type(invalid_model_type):
     [test_string, test_int, test_float, test_list, test_dict, test_tuple],
 )
 def test_setup_plugin_instance_with_invalid_logger_type(invalid_logger_type):
-    test_object = TestObject()
+    test_object = ObjectTest()
     test_object._input_args["logger"] = invalid_logger_type
     expected_exception_msg = (
         "The algorithm has failed to set up logger. The logger type is invalid"
@@ -411,7 +416,7 @@ def test_setup_plugin_instance_with_invalid_logger_type(invalid_logger_type):
     ],
 )
 def test_setup_plugin_instance_with_invalid_log_level_and_message(log_info):
-    test_object = TestObject()
+    test_object = ObjectTest()
     expected_exception_msg = "The algorithm has invalid log level or message."
     test_plugin = Plugin(
         test_object._data_instance_and_serializer,
@@ -432,7 +437,7 @@ def test_setup_plugin_instance_with_invalid_log_level_and_message(log_info):
     [test_string, test_int, test_float, test_list, test_dict, test_tuple],
 )
 def test_init_plugin_instance_with_invalid_project_base_path(invalid_project_base_path):
-    test_object = TestObject()
+    test_object = ObjectTest()
     test_object._input_args["project_base_path"] = invalid_project_base_path
     expected_exception_msg = "The algorithm has failed validation for the project path."
     with pytest.raises(test_object._expected_exception) as excinfo:
@@ -449,7 +454,7 @@ def test_init_plugin_instance_with_invalid_project_base_path(invalid_project_bas
 
 
 def test_plugin_valid_get_metadata():
-    test_object = TestObject()
+    test_object = ObjectTest()
     test_plugin = Plugin(
         test_object._data_instance_and_serializer,
         test_object._model_instance_and_serializer,
@@ -462,7 +467,7 @@ def test_plugin_valid_get_metadata():
 
 
 def test_plugin_valid_get_plugin_type():
-    test_object = TestObject()
+    test_object = ObjectTest()
     test_plugin = Plugin(
         test_object._data_instance_and_serializer,
         test_object._model_instance_and_serializer,
@@ -480,7 +485,7 @@ def test_plugin_valid_get_plugin_type():
     indirect=["get_data_instance_and_serializer_without_ground_truth"],
 )
 def test_valid_run(get_data_instance_and_serializer_without_ground_truth):
-    test_object = TestObject()
+    test_object = ObjectTest()
     test_object._ground_truth_instance_and_serializer[0].keep_ground_truth(
         test_object._ground_truth
     )
@@ -504,7 +509,7 @@ def test_valid_run(get_data_instance_and_serializer_without_ground_truth):
     print("my results", results)
 
     # Load sample JSON file to assert results
-    f = open("tests/user_defined_files/unit_tests/sample_output.json")
+    f = open(str(Path(__file__).parent / "sample_output.json"))
     sample_data = json.load(f)
     f.close()
 
