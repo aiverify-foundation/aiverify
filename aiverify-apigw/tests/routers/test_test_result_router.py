54492b799ab8dfed50d396dc53f6c50cbeac7aca
Peck Yoke <leong_peck_yoke@imda.gov.sg>
2024-09-24 10:47:16 +0800

    add more test cases for upload

--- aiverify-apigw/tests/routers/test_test_result_router.py
+++ aiverify-apigw/tests/routers/test_test_result_router.py
@@ -2,7 +2,7 @@ import pytest
 from fastapi.testclient import TestClient
 from unittest.mock import patch, MagicMock
 from aiverify_apigw.__main__ import app
-from aiverify_apigw.models import TestResultModel, TestArtifactModel
+from aiverify_apigw.models import TestResultModel, TestArtifactModel, TestDatasetModel, TestModelModel
 import json
 
 client = TestClient(app)
@@ -26,17 +26,15 @@ class TestUploadTestResult:
                 "groundTruthDataset": "file:///examples/data/sample_bc_credit_data.sav",
                 "groundTruth": "default",
                 "algorithmArgs": {
-                    "sensitive_feature": [
+                    "arg1": [
                         "gender"
-                    ],
-                    "annotated_labels_path": "file:///examples/data/sample_bc_credit_data.sav",
-                    "file_name_label": "NA"
+                    ]
                 },
                 "mode": "upload",
                 "modelType": "classification"
             },
             "output": {
-                "result": "success"
+                "result": 100
             }
         }
 
@@ -47,6 +45,35 @@ class TestUploadTestResult:
         mock_file.content_type = "text/plain"
         mock_file.file.read.return_value = b"Hello World"
         yield mock_file
+
+    def test_upload_test_result_algorithm_not_found(self, mock_test_result_data, test_client):
+        """Test POST /test_result/upload when the algorithm is not found."""
+        form_data = {
+            "test_result": json.dumps(mock_test_result_data)
+        }
+        response = test_client.post("/test_result/upload", data=form_data)
+        assert response.status_code == 400
+
+    def test_upload_test_result_invalid_output_schema(self, mock_test_result_data, test_client, db_session, mock_plugins):
+        """Test the POST /test_result/upload route when test result output schema is invalid."""
+
+        # files = {'artifacts': (None, None)}  # No files uploaded
+        plugin = mock_plugins[0]
+        algo = plugin.algorithms[0]
+
+        mock_test_result_data["gid"] = plugin.gid
+        mock_test_result_data["cid"] = algo.cid
+        mock_test_result_data["output"] = {"fake": 100}
+        form_data = {
+            "test_result": json.dumps(mock_test_result_data)
+        }
+        print("form data: ", form_data)
+        response = test_client.post("/test_result/upload", data=form_data)
+        assert response.status_code == 422
+
+        assert db_session.query(TestResultModel).count() == 0
+        assert db_session.query(TestModelModel).count() == 0
+        assert db_session.query(TestDatasetModel).count() == 0
 
     def test_upload_test_result_no_files_success(self, mock_test_result_data, test_client, db_session, mock_plugins):
         """Test the POST /test_result/upload route for successful upload with no files uploaded."""
@@ -63,8 +90,9 @@ class TestUploadTestResult:
         response = test_client.post("/test_result/upload", data=form_data)
         assert response.status_code == 200
 
-        count = db_session.query(TestResultModel).count()
-        assert count == 1
+        assert db_session.query(TestResultModel).count() == 1
+        assert db_session.query(TestModelModel).count() == 1
+        assert db_session.query(TestDatasetModel).count() == 1
 
     @patch("aiverify_apigw.routers.test_result_router.save_artifact")
     def test_upload_test_result_with_files_success(self, mock_save_artifact, mock_test_result_data, test_client, db_session, mock_plugins, mock_upload_file):
@@ -76,7 +104,7 @@ class TestUploadTestResult:
 
         mock_test_result_data["gid"] = plugin.gid
         mock_test_result_data["cid"] = algo.cid
-        mock_test_result_data["artifacts"] = [mock_upload_file.filename]
+        # mock_test_result_data["artifacts"] = [mock_upload_file.filename]
         files = {'artifacts': (mock_upload_file.filename, mock_upload_file.file.read(), mock_upload_file.content_type)}
         form_data = {
             "test_result": json.dumps(mock_test_result_data)
@@ -89,16 +117,47 @@ class TestUploadTestResult:
         )
 
         assert response.status_code == 200
-        mock_save_artifact.assert_called_once()
-        count = db_session.query(TestResultModel).count()
-        assert count == 2  # add 1 from previous run
-        count = db_session.query(TestArtifactModel).count()
-        assert count == 1
+        mock_save_artifact.assert_not_called()  # not called because artifact not listed
+        assert db_session.query(TestResultModel).count() == 2  # add 1 from previous run
+        assert db_session.query(TestArtifactModel).count() == 0
+        assert db_session.query(TestModelModel).count() == 1
+        assert db_session.query(TestDatasetModel).count() == 1
 
-    def test_upload_test_result_algorithm_not_found(self, mock_test_result_data, test_client):
-        """Test POST /test_result/upload when the algorithm is not found."""
+        # call again with aritifacts set
+        mock_test_result_data["artifacts"] = [mock_upload_file.filename]
+        form_data = {
+            "test_result": json.dumps(mock_test_result_data)
+        }
+        response = test_client.post(
+            "/test_result/upload",
+            # headers={"Content-Type": "multipart/form-data"},
+            data=form_data,
+            files=files
+        )
+
+        assert response.status_code == 200
+        mock_save_artifact.assert_called_once()
+        assert db_session.query(TestResultModel).count() == 3  # add 1 from previous run
+        assert db_session.query(TestArtifactModel).count() == 1
+        assert db_session.query(TestModelModel).count() == 1
+        assert db_session.query(TestDatasetModel).count() == 1
+
+    def test_upload_test_result_new_ground_truth(self, mock_test_result_data, test_client, db_session, mock_plugins):
+        """Test the POST /test_result/upload route for new ground truth dataset created."""
+
+        # files = {'artifacts': (None, None)}  # No files uploaded
+        plugin = mock_plugins[0]
+        algo = plugin.algorithms[0]
+
+        mock_test_result_data["gid"] = plugin.gid
+        mock_test_result_data["cid"] = algo.cid
+        mock_test_result_data["test_arguments"]["groundTruthDataset"] = "file:///examples/data/sample_ground_truth.sav"
         form_data = {
             "test_result": json.dumps(mock_test_result_data)
         }
         response = test_client.post("/test_result/upload", data=form_data)
-        assert response.status_code == 400
+        assert response.status_code == 200
+
+        assert db_session.query(TestResultModel).count() == 4
+        assert db_session.query(TestModelModel).count() == 1
+        assert db_session.query(TestDatasetModel).count() == 2
