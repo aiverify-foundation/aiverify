d2092bb9255e6fcb69fad262c60a366ca13c9cd3
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-10-10 12:16:03 +0800

    update lint and add prettier

--- aiverify-portal/app/homepage.tsx
+++ aiverify-portal/app/homepage.tsx
@@ -1,15 +1,17 @@
-'use client'
+'use client';
 
-import { Modal } from "@/components/modal";
+import { Modal } from '@/components/modal';
 
 export function Homepage() {
-  return       <Modal
-  heading='New AIV work in progress'
-  enableScreenOverlay={false}
-  height={250}
-  primaryBtnLabel='Hello World'
-  onCloseIconClick={() => null}
-  onPrimaryBtnClick={() => alert('hello world')}>
-    <p>Using Next.js</p>
-  </Modal>
+  return (
+    <Modal
+      heading="New AIV work in progress"
+      enableScreenOverlay={false}
+      height={250}
+      primaryBtnLabel="Hello World"
+      onCloseIconClick={() => null}
+      onPrimaryBtnClick={() => alert('hello world')}>
+      <p>Using Next.js</p>
+    </Modal>
+  );
 }
