1bd9f43c2574346ee11bf9d53c9a1544bcb3bf8d
Peck Yoke <leong_peck_yoke@imda.gov.sg>
2024-11-13 22:55:01 +0800

    add schemas

--- aiverify-apigw/aiverify_apigw/schemas/input_block.py
+++ aiverify-apigw/aiverify_apigw/schemas/input_block.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, Field
-from typing import List, Optional, Literal, Annotated
+from typing import List, Optional, Annotated
+from ..models import InputBlockModel
+from ..lib.constants import InputBlockSize
 
 
 class InputBlockMeta(BaseModel):
@@ -22,7 +24,32 @@ class InputBlockMeta(BaseModel):
     )
     description: Optional[str] = Field(default=None, description="Input Block description", max_length=256)
     group: Optional[str] = Field(default=None, description="Input Block group", min_length=1, max_length=128)
-    width: Optional[Literal["xs", "sm", "md", "lg", "xl"]] = Field(
+    width: Optional[InputBlockSize] = Field(
         default="md", description="Width of Input Block dialog"
     )
     fullScreen: Optional[bool] = Field(default=False, description="Width of Input Block dialog")
+
+
+class InputBlockOutput(InputBlockMeta):
+    gid: Optional[str] = Field(
+        description="Unique global identifier for the plugin",
+        min_length=1,
+        max_length=128,
+        pattern=r"^[a-zA-Z0-9][a-zA-Z0-9-._]*$",
+    )
+
+    @classmethod
+    def from_model(cls, result: InputBlockModel) -> "InputBlockOutput":
+        obj = cls(
+            cid=result.cid,
+            gid=result.plugin.gid,
+            name=result.name,
+            version=result.version,
+            author=result.author,
+            tags=[str(tag.name) for tag in result.tags] if result.tags else None,
+            description=result.description,
+            group=result.group,
+            width=result.width,
+            fullScreen=result.fullscreen
+        )
+        return obj
