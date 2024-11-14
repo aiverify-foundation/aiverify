c4558ed639cbbb6e1904e05fe48608af0be42346
Peck Yoke <leong_peck_yoke@imda.gov.sg>
2024-11-14 14:16:14 +0800

    add new mocks for widgets, input blocks and algorithms

--- aiverify-apigw/tests/mocks/mock_data_plugin.py
+++ aiverify-apigw/tests/mocks/mock_data_plugin.py
@@ -2,8 +2,8 @@
 from faker import Faker
 from typing import List
 import json
-from aiverify_apigw.models import PluginModel, AlgorithmModel
-
+from aiverify_apigw.models import PluginModel, AlgorithmModel, WidgetModel, InputBlockModel, TemplateModel
+from aiverify_apigw.lib.constants import InputBlockSize
 faker = Faker()
 
 
@@ -46,11 +46,90 @@ def _create_mock_algorithm(gid: str):
     )
 
 
-def _create_mock_plugin(num_algo: int | None = None, is_stock: bool = True):
+def _create_mock_widgets(gid: str):
+    cid = ".".join(faker.words()).lower()
+    widget_size = json.dumps({
+            "minW": faker.random_int(min=1, max=12),
+            "minH": faker.random_int(min=1, max=12),
+            "maxW": faker.random_int(min=1, max=12),
+            "maxH": faker.random_int(min=1, max=12),
+        }).encode("utf-8")
+    properties = json.dumps([json.dumps({"key": faker.word(), "helper": faker.word()})]).encode("utf-8")
+    # mockdata = json.dumps({"mockKey": faker.word(), "mockValue": faker.word()}).encode("utf-8")
+    dependencies = json.dumps([json.dumps({"cid": faker.word()})]).encode("utf-8")
+    return WidgetModel(
+        id=f"{gid}:{cid}",
+        cid=cid,
+        name=faker.name(),
+        version=faker.numerify("%!!.%!!.%!!"),
+        author=faker.name(),
+        description=faker.text(max_nb_chars=256),
+        gid=gid,
+        meta=b'{"key1":"value1"}',
+        widget_size=widget_size,
+        properties=properties,
+        # mockdata=mockdata,
+        dynamic_height=faker.boolean(),
+        dependencies=dependencies,
+        plugin_id=gid,
+    )
+
+
+def _create_mock_input_block(gid: str):
+    cid = ".".join(faker.words()).lower()
+    group = faker.word()
+    width = faker.random_element(elements=[InputBlockSize.xs, InputBlockSize.sm,
+                                 InputBlockSize.md, InputBlockSize.lg, InputBlockSize.xl])
+    fullscreen = faker.boolean()
+    return InputBlockModel(
+        id=f"{gid}:{cid}",
+        cid=cid,
+        name=faker.name(),
+        version=faker.numerify("%!!.%!!.%!!"),
+        author=faker.name(),
+        description=faker.text(max_nb_chars=256),
+        gid=gid,
+        meta=b'{"key1":"value1"}',
+        group=group,
+        width=width,
+        fullscreen=fullscreen,
+        plugin_id=gid,
+    )
+
+
+def _create_mock_template(gid: str):
+    cid = ".".join(faker.words()).lower()
+    template = json.dumps({"templateKey": faker.word(), "templateValue": faker.word()}).encode("utf-8")
+    project_data = json.dumps({"projectKey": faker.word(), "projectValue": faker.word()}).encode("utf-8")
+    return TemplateModel(
+        id=f"{gid}:{cid}",
+        cid=cid,
+        name=faker.name(),
+        version=faker.numerify("%!!.%!!.%!!"),
+        author=faker.name(),
+        description=faker.text(max_nb_chars=256),
+        gid=gid,
+        meta=b'{"key1":"value1"}',
+        template=template,
+        project_data=project_data,
+        plugin_id=gid,
+    )
+
+
+def _create_mock_plugin(num_algo: int | None = None, num_widgets: int | None = None, num_input_blocks: int | None = None, num_templates: int | None = None, is_stock: bool = True):
     gid = ".".join(faker.words()).lower()
     if num_algo is None:
         num_algo = faker.random_int(min=1, max=3)
     algorithms = [_create_mock_algorithm(gid) for i in range(num_algo)]
+    if num_widgets is None:
+        num_widgets = faker.random_int(min=1, max=3)
+    widgets = [_create_mock_widgets(gid) for i in range(num_widgets)]
+    if num_input_blocks is None:
+        num_input_blocks = faker.random_int(min=1, max=3)
+    inputblocks = [_create_mock_input_block(gid) for i in range(num_input_blocks)]
+    if num_templates is None:
+        num_templates = faker.random_int(min=1, max=2)
+    templates = [_create_mock_template(gid) for i in range(num_templates)]
     meta = {
         "gid": gid,
         "version": faker.numerify("%!!.%!!.%!!"),  # Generates a semantic version string
@@ -69,6 +148,9 @@ def _create_mock_plugin(num_algo: int | None = None, is_stock: bool = True):
         meta=json.dumps(meta).encode("utf-8"),
         is_stock=is_stock,
         algorithms=algorithms,
+        widgets=widgets,
+        inputblocks=inputblocks,
+        templates=templates,
     )
 
 
