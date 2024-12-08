afc95db8acb57a2a9550b524f949686d10b73b37
Peck Yoke <leong_peck_yoke@imda.gov.sg>
2024-12-08 21:26:25 +0800

    update apis for plugin templates

--- aiverify-apigw/aiverify_apigw/__main__.py
+++ aiverify-apigw/aiverify_apigw/__main__.py
@@ -9,7 +9,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 from .lib.logging import logger
-from .routers import test_result_router, plugin_router
+from .routers import project_template_router, test_result_router, plugin_router
 from .lib.database import engine
 from .models import BaseORMModel
 from .lib.plugin_store import PluginStore
@@ -23,6 +23,7 @@ app = FastAPI()
 # include routers
 app.include_router(test_result_router.router)
 app.include_router(plugin_router.router)
+app.include_router(project_template_router.router)
 
 
 # modify the openai schema
---
+++ aiverify-apigw/aiverify_apigw/routers/project_template_router.py
@@ -0,0 +1,123 @@
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session, make_transient
+from typing import List
+from datetime import datetime
+
+from ..lib.logging import logger
+from ..lib.database import get_db_session
+from ..models import ProjectTemplateModel
+from ..schemas import ProjectTemplateInput, ProjectTemplateOutput, ProjectTemplateMeta
+
+router = APIRouter(prefix="/project_templates", tags=["project_template"])
+
+
+@router.get("/", response_model=List[ProjectTemplateOutput])
+async def get_all_project_templates(session: Session = Depends(get_db_session)):
+    """
+    Get all Project Templates
+    """
+    try:
+        project_templates = session.query(ProjectTemplateModel).all()
+        return [ProjectTemplateOutput.from_model(template) for template in project_templates]
+    except Exception as e:
+        return HTTPException(status_code=500, detail=f"Internal error {e}")
+
+
+@router.post("/", response_model=ProjectTemplateOutput)
+async def create_project_template(
+    projectTemplate: ProjectTemplateInput, session: Session = Depends(get_db_session)
+):
+    """
+    Create a new Project Template
+    """
+    try:
+        meta = ProjectTemplateMeta(globalVars=projectTemplate.globalVars, pages=projectTemplate.pages)
+        now = datetime.now()
+        new_template = ProjectTemplateModel(
+            name=projectTemplate.projectInfo.name,
+            description=projectTemplate.projectInfo.description,
+            data=meta.model_dump_json().encode("utf-8"),
+            created_at=now,
+            updated_at=now
+        )
+        session.add(new_template)
+        session.commit()
+        session.refresh(new_template)
+        return ProjectTemplateOutput.from_model(new_template)
+    except Exception as e:
+        session.rollback()
+        logger.error(f"Error creating project template: {e}")
+        raise HTTPException(status_code=500, detail=f"Internal error {e}")
+
+
+@router.get("/{template_id}", response_model=ProjectTemplateOutput)
+async def get_project_template_by_id(template_id: int, session: Session = Depends(get_db_session)):
+    """
+    Get a Project Template by ID
+    """
+    try:
+        template = session.query(ProjectTemplateModel).filter(ProjectTemplateModel.id == template_id).first()
+        if not template:
+            raise HTTPException(status_code=404, detail="Project Template not found")
+        return ProjectTemplateOutput.from_model(template)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        logger.error(f"Error retrieving project template with ID {template_id}: {e}")
+        raise HTTPException(status_code=500, detail=f"Internal error {e}")
+
+
+@router.delete("/{template_id}")
+async def delete_project_template(template_id: int, session: Session = Depends(get_db_session)):
+    """
+    Delete a Project Template by ID
+    """
+    try:
+        template = session.query(ProjectTemplateModel).filter(ProjectTemplateModel.id == template_id).first()
+        if not template:
+            raise HTTPException(status_code=404, detail="Project Template not found")
+        
+        if template.from_plugin:
+            raise HTTPException(status_code=400, detail="Plugin templates are not allowed to be deleted")
+
+        session.delete(template)
+        session.commit()
+        return template_id
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        session.rollback()
+        logger.error(f"Error deleting project template with ID {template_id}: {e}")
+        raise HTTPException(status_code=500, detail=f"Internal error {e}")
+
+
+@router.post("/clone/{template_id}", response_model=ProjectTemplateOutput)
+async def clone_project_template(template_id: int, session: Session = Depends(get_db_session)):
+    """
+    Clone a Project Template by ID
+    """
+    try:
+        template = session.query(ProjectTemplateModel).filter(ProjectTemplateModel.id == template_id).first()
+        if not template:
+            raise HTTPException(status_code=404, detail="Project Template not found")
+
+        now = datetime.now()
+        new_template = ProjectTemplateModel(
+            name=f"Copy of {template.name}",
+            description=template.description,
+            data=template.data,
+            plugin_id=None,  # cloned templates are not linked to plugins
+            created_at=now,
+            updated_at=now
+        )
+        session.add(new_template)
+        session.commit()
+        session.refresh(new_template)
+        return ProjectTemplateOutput.from_model(new_template)
+
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        session.rollback()
+        logger.error(f"Error cloning project template with ID {template_id}: {e}")
+        raise HTTPException(status_code=500, detail=f"Internal error {e}")
