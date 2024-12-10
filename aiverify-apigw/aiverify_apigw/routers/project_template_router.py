50d9cb432e5668e01beb4b5e62942e0ad7a0b373
Peck Yoke <leong_peck_yoke@imda.gov.sg>
2024-12-10 15:29:08 +0800

    add patch update

--- aiverify-apigw/aiverify_apigw/routers/project_template_router.py
+++ aiverify-apigw/aiverify_apigw/routers/project_template_router.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
-from typing import List
-from datetime import datetime
+from typing import List, Optional
+from datetime import datetime, timezone
+# from uuid import uuid4
 
 from ..lib.logging import logger
 from ..lib.database import get_db_session
 from ..models import ProjectTemplateModel
-from ..schemas import ProjectTemplateInput, ProjectTemplateOutput, ProjectTemplateMeta
+from ..schemas import ProjectTemplateInput, ProjectTemplatePatchInput, ProjectTemplateOutput, ProjectTemplateMeta, ProjectTemplateInformationOptional
 
 router = APIRouter(prefix="/project_templates", tags=["project_template"])
 
@@ -34,6 +35,7 @@ async def create_project_template(
         meta = ProjectTemplateMeta(globalVars=projectTemplate.globalVars, pages=projectTemplate.pages)
         now = datetime.now()
         new_template = ProjectTemplateModel(
+            # id=str(uuid4()),
             name=projectTemplate.projectInfo.name,
             description=projectTemplate.projectInfo.description,
             data=meta.model_dump_json().encode("utf-8"),
@@ -51,7 +53,7 @@ async def create_project_template(
 
 
 @router.get("/{template_id}", response_model=ProjectTemplateOutput)
-async def get_project_template_by_id(template_id: int, session: Session = Depends(get_db_session)):
+async def get_project_template_by_id(template_id: str, session: Session = Depends(get_db_session)):
     """
     Get a Project Template by ID
     """
@@ -68,8 +70,8 @@ async def get_project_template_by_id(template_id: int, session: Session = Depend
 
 
 @router.put("/{template_id}", response_model=ProjectTemplateOutput)
-async def update_project_template(
-    template_id: int, projectTemplate: ProjectTemplateInput, session: Session = Depends(get_db_session)
+async def patch_project_template(
+    template_id: str, projectTemplate: ProjectTemplateInput, session: Session = Depends(get_db_session)
 ):
     """
     Update a Project Template by ID
@@ -98,8 +100,48 @@ async def update_project_template(
         raise HTTPException(status_code=500, detail=f"Internal error {e}")
 
 
+@router.patch("/{template_id}", response_model=ProjectTemplateOutput)
+async def update_project_template(
+    template_id: str, projectTemplate: ProjectTemplatePatchInput, session: Session = Depends(get_db_session)
+):
+    """
+    Patch update a Project Template by ID
+    """
+    try:
+        template = session.query(ProjectTemplateModel).filter(ProjectTemplateModel.id == template_id).first()
+        if not template:
+            raise HTTPException(status_code=404, detail="Project Template not found")
+        if template.from_plugin:
+            raise HTTPException(status_code=400, detail="Plugin templates are not allowed to be edited")
+        
+        update_data = projectTemplate.model_dump(exclude_unset=True)
+        logger.debug(f"update_project_template update data: {update_data}")
+
+        if "globalVars" in update_data or "pages" in update_data:
+            update_meta = ProjectTemplateMeta.model_validate_json(template.data.decode("utf-8"))
+            if "globalVars" in update_data:
+                update_meta.globalVars = update_data["globalVars"]
+            if "pages" in update_data:
+                update_meta.pages = update_data["pages"]
+            template.data = update_meta.model_dump_json().encode("utf-8")
+        if "projectInfo" in update_data:
+            template.name = update_data["projectInfo"]["name"]
+            template.description = update_data["projectInfo"]["description"]
+        template.updated_at = datetime.now(timezone.utc)
+
+        session.commit()
+        session.refresh(template)
+        return ProjectTemplateOutput.from_model(template)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        session.rollback()
+        logger.error(f"Error updating project template with ID {template_id}: {e}")
+        raise HTTPException(status_code=500, detail=f"Internal error {e}")
+
+
 @router.delete("/{template_id}")
-async def delete_project_template(template_id: int, session: Session = Depends(get_db_session)):
+async def delete_project_template(template_id: str, session: Session = Depends(get_db_session)):
     """
     Delete a Project Template by ID
     """
@@ -123,7 +165,7 @@ async def delete_project_template(template_id: int, session: Session = Depends(g
 
 
 @router.post("/clone/{template_id}", response_model=ProjectTemplateOutput)
-async def clone_project_template(template_id: int, session: Session = Depends(get_db_session)):
+async def clone_project_template(template_id: str, projectInfo: Optional[ProjectTemplateInformationOptional]=None, session: Session = Depends(get_db_session)):
     """
     Clone a Project Template by ID
     """
@@ -132,12 +174,13 @@ async def clone_project_template(template_id: int, session: Session = Depends(ge
         if not template:
             raise HTTPException(status_code=404, detail="Project Template not found")
 
-        now = datetime.now()
+        now = datetime.now(timezone.utc)
         new_template = ProjectTemplateModel(
-            name=f"Copy of {template.name}",
-            description=template.description,
+            # id=str(uuid4()),
+            name=projectInfo.name if projectInfo and projectInfo.name else f"Copy of {template.name}",
+            description=projectInfo.description if projectInfo and projectInfo.description else template.description,
             data=template.data,
-            plugin_id=None,  # cloned templates are not linked to plugins
+            template_id=None,  # cloned templates are not linked to plugin template
             created_at=now,
             updated_at=now
         )
