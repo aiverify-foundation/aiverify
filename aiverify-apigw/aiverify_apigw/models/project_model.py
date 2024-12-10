e84c3cab6004fd8a935ae213e1324b0fe59b000f
Peck Yoke <leong_peck_yoke@imda.gov.sg>
2024-12-10 15:26:02 +0800

    add project model classes

--- aiverify-apigw/aiverify_apigw/models/project_model.py
+++ aiverify-apigw/aiverify_apigw/models/project_model.py
@@ -1,43 +1,28 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import ForeignKey
-from sqlalchemy.ext.hybrid import hybrid_property
-from typing import Optional
-from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, Table, Column
+from typing import Optional, List
 
 from .base_model import BaseORMModel
+from .project_template_model import ProjectBaseModel, ProjectTemplateModel
+from .test_model_model import TestModelModel
+from .input_block_data_model import InputBlockDataModel
+from .test_result_model import TestResultModel
+from .user_model import UserModel
 
 
-class ProjectBaseModel:
-    # __tablename__ = "project_base"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str]
-    description: Mapped[Optional[str]]
-    data: Mapped[bytes]
-    created_at: Mapped[Optional[datetime]]
-    updated_at: Mapped[Optional[datetime]]
-    # type: Mapped[str]
+input_blocks_association_table = Table(
+    "project_input_blocks_association",
+    BaseORMModel.metadata,
+    Column("project_id", ForeignKey("project.id")),
+    Column("input_block_data_id", ForeignKey("inputblock_data.id")),
+)
 
-    # __mapper_args__ = {
-    #     'polymorphic_identity': 'project_base',
-    #     'polymorphic_on': "type"
-    # }
-
-
-class ProjectTemplateModel(BaseORMModel, ProjectBaseModel):
-    __tablename__ = "project_template"
-
-    plugin_id: Mapped[Optional[str]] = mapped_column(ForeignKey("plugin.gid"))
-
-    @hybrid_property
-    def from_plugin(self):
-        return self.plugin_id is not None
-
-    # plugin = relationship("PluginModel", foreign_keys=[plugin_id])
-    # base = relationship("ProjectBaseModel", foreign_keys=[id])
-
-    # __mapper_args__ = {
-    #     'polymorphic_identity': 'project_template',
-    # }
+test_results_association_table = Table(
+    "project_test_results_association",
+    BaseORMModel.metadata,
+    Column("project_id", ForeignKey("project.id")),
+    Column("test_result_id", ForeignKey("test_result.id")),
+)
 
 
 class ProjectModel(BaseORMModel, ProjectBaseModel):
@@ -47,8 +32,16 @@ class ProjectModel(BaseORMModel, ProjectBaseModel):
     company: Mapped[Optional[str]]
 
     # template ID should be using the format {gid}:{cid}, if not imported from any template, set to None
-    template_id: Mapped[Optional[str]] = mapped_column(default=None)
+    template_id: Mapped[Optional[str]] = mapped_column(ForeignKey("project_template.id"))
+    template: Mapped[Optional["ProjectTemplateModel"]] = relationship()
 
-    # __mapper_args__ = {
-    #     'polymorphic_identity': 'project',
-    # }
+    test_model_id: Mapped[Optional[int]] = mapped_column(ForeignKey("test_model.id"))
+    test_model: Mapped[Optional["TestModelModel"]] = relationship() # should be automatically set to None if referenced test model is delete
+
+    # list of selected input blocks
+    input_blocks_data: Mapped[List[InputBlockDataModel]] = relationship(secondary=input_blocks_association_table)
+    # list of selected test results
+    test_results: Mapped[List[TestResultModel]] = relationship(secondary=test_results_association_table)
+
+    user_id: Mapped[Optional[int]] = mapped_column(ForeignKey("user.id"))
+    user: Mapped[Optional["UserModel"]] = relationship()
