fa8326d2acfdc9b9f14ff0664da863909928025f
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-12-11 10:28:15 +0800

    disable upload btn and update react 19 ref prop

--- aiverify-portal/app/results/upload/components/uploader.tsx
+++ aiverify-portal/app/results/upload/components/uploader.tsx
@@ -2,16 +2,17 @@
 
 import { useMutation } from '@tanstack/react-query';
 import dynamic from 'next/dynamic';
+import Link from 'next/link';
 import React from 'react';
 import { useRef, useState } from 'react';
 import { Button, ButtonVariant } from '@/lib/components/button';
 import { Modal } from '@/lib/components/modal';
+import { debounce } from '@/lib/utils/debounce';
 import { cn } from '@/lib/utils/twmerge';
 import { FileSelector, FileSelectorHandle } from './fileSelector';
 import { JsonEditorHandle } from './jsoneditor';
 import { FileUpload } from './types';
 import { uploadTestResult } from './utils/testResultUploadRequest';
-import Link from 'next/link';
 
 const JsonEditor = dynamic(
   () => import('./jsoneditor').then((mod) => mod.JsonEditor),
@@ -27,10 +28,10 @@ export function Uploader({ className }: { className?: string }) {
   const editorRef = useRef<JsonEditorHandle>(null);
   const fileSelectorRef = useRef<FileSelectorHandle>(null);
   const [successData, setSuccessData] = useState<string[] | null>(null);
+  const [disableUpload, setDisableUpload] = useState(true);
   const upload = useMutation({
     mutationFn: uploadTestResult,
     onSuccess: (result) => {
-      console.log(result);
       setSuccessData(result.data ?? null);
     },
     onError: (error) => {
@@ -48,19 +49,32 @@ export function Uploader({ className }: { className?: string }) {
     const jsonData = editorRef.current?.getValue() ?? {};
     const files = fileSelectorRef.current?.getFiles() ?? [];
 
-    // if (!files.length) {
-    //   console.error('No files selected');
-    //   return;
-    // }
-
     try {
       await upload.mutateAsync({
         jsonData,
         files,
       });
     } catch (error) {
-      // Error handling is done in mutation callbacks
       console.error('Upload failed:', error);
+    }
+  }
+
+  const debouncedHandleEditorChange = debounce(() => {
+    setDisableUpload(false);
+  }, 200);
+
+  const debouncedHandleSyntaxError = debounce((error: string) => {
+    console.log(error);
+    setDisableUpload(true);
+  }, 200);
+
+  function isJsonValid(value: string) {
+    try {
+      JSON.parse(value);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
   }
 
@@ -89,6 +103,8 @@ export function Uploader({ className }: { className?: string }) {
         <div className="flex w-[75%] flex-col justify-between">
           <JsonEditor
             className="h-[600px]"
+            onChange={debouncedHandleEditorChange}
+            onSyntaxError={debouncedHandleSyntaxError}
             ref={editorRef}
           />
           <div className="flex justify-end">
@@ -98,6 +114,7 @@ export function Uploader({ className }: { className?: string }) {
               text="Upload"
               className="mt-8"
               onClick={handleUpload}
+              disabled={disableUpload}
             />
           </div>
         </div>
