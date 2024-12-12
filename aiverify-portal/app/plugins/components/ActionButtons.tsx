5e317aa781105d9546a702c4cff1e29aa08381b3
Chong Wan Fei <chong_wan_fei@imda.gov.sg>
2024-12-12 17:14:01 +0800

    added upload page

--- aiverify-portal/app/plugins/components/ActionButtons.tsx
+++ aiverify-portal/app/plugins/components/ActionButtons.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Button, ButtonVariant } from '@/lib/components/button';
+import Link from 'next/link';
 
 const ActionButtons: React.FC = () => (
   <div className="flex">
-    <Button
-      pill
-      textColor="white"
-      variant={ButtonVariant.OUTLINE}
-      size="sm"
-      text="IMPORT PLUGIN"
-    />
+    <Link href="/plugins/upload">
+      <Button
+        pill
+        textColor="white"
+        variant={ButtonVariant.OUTLINE}
+        size="sm"
+        text="UPLOAD PLUGIN"
+      />
+    </Link>
   </div>
 );
 
---
+++ aiverify-portal/app/plugins/upload/page.tsx
@@ -0,0 +1,10 @@
+function UploadPage() {
+    return (
+      <main className="h-screen w-full px-6">
+        <h1 className="my-6 text-2xl font-bold tracking-wide">Upload Page</h1>
+      </main>
+    );
+  }
+  
+  export default UploadPage;
+  
