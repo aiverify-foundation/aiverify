f0b079c1d2a4c13a47544fa6c744b05b738cb663
Peck Yoke <leong_peck_yoke@imda.gov.sg>
2024-12-13 15:30:48 +0800

    remove uploaded_file_model and all references

--- aiverify-apigw/aiverify_apigw/models/test_dataset_model.py
+++ aiverify-apigw/aiverify_apigw/models/test_dataset_model.py
@@ -1,9 +1,8 @@
-from sqlalchemy import Enum, String, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Enum, String
+from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime
 from typing import Optional
 from .base_model import BaseORMModel
-from .uploaded_file_model import UploadedFileModel
 from ..lib.constants import TestDatasetFileType, TestDatasetStatus
 
 
@@ -14,12 +13,9 @@ class TestDatasetModel(BaseORMModel):
     name: Mapped[str] = mapped_column(String(128), nullable=False)
     description: Mapped[Optional[str]]
     status: Mapped[TestDatasetStatus] = mapped_column(Enum(TestDatasetStatus), nullable=False)
-    datapath_id: Mapped[Optional[int]] = mapped_column(ForeignKey("uploaded_file.id"))
-    datapath: Mapped[Optional["UploadedFileModel"]] = relationship(
-        "UploadedFileModel", cascade="save-update"
-    )  # relative to datepath. if not uploaded, leave as null
-    filepath: Mapped[str] = mapped_column(String(2048), nullable=False)
+    # filepath: Mapped[str] = mapped_column(String(2048), nullable=False)
     filename: Mapped[str] = mapped_column(String(2048), nullable=False, index=True)
+    zip_hash: Mapped[Optional[str]]
     file_type: Mapped[TestDatasetFileType] = mapped_column(Enum(TestDatasetFileType))
     error_message: Mapped[Optional[str]]  # only for status == invalid
     size: Mapped[Optional[float]]
--- aiverify-apigw/aiverify_apigw/models/test_model_model.py
+++ aiverify-apigw/aiverify_apigw/models/test_model_model.py
@@ -1,9 +1,8 @@
-from sqlalchemy import String, Enum, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Enum
+from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime
 from typing import Optional
 from .base_model import BaseORMModel
-from .uploaded_file_model import UploadedFileModel
 from ..lib.constants import TestModelFileType, TestModelMode, TestModelStatus, ModelType
 
 
@@ -17,16 +16,12 @@ class TestModelModel(BaseORMModel):
     file_type: Mapped[Optional[TestModelFileType]] = mapped_column(Enum(TestModelFileType))  # only if mode == upload
     model_type: Mapped[ModelType] = mapped_column(Enum(ModelType), nullable=False)
     status: Mapped[TestModelStatus] = mapped_column(Enum(TestModelStatus), nullable=False)
-    size: Mapped[Optional[float]]
+    size: Mapped[Optional[int]] # if None or zero, means not uploaded 
     serializer: Mapped[Optional[str]]  # only if mode == upload
     model_format: Mapped[Optional[str]]  # only if mode == upload
     # relative to datepath. if not uploaded, leave as null
-    datapath_id: Mapped[Optional[int]] = mapped_column(ForeignKey("uploaded_file.id"))
-    datapath: Mapped[Optional["UploadedFileModel"]] = relationship(
-        "UploadedFileModel", cascade="save-update"
-    )  # relative to datepath. if not uploaded, leave as null
-    filepath: Mapped[Optional[str]] = mapped_column(String(2048))  # only if mode == upload
-    filename: Mapped[Optional[str]] = mapped_column(String(2048), index=True)  # only if mode == upload
+    filename: Mapped[Optional[str]] = mapped_column(String(2048), index=True, unique=True)  # only if mode == upload
+    zip_hash: Mapped[Optional[str]]
     model_api: Mapped[Optional[bytes]]  # serialized json, only if model == api
     error_message: Mapped[Optional[str]]  # only for status == invalid
     # date times
@@ -42,14 +37,14 @@ class TestModelModel(BaseORMModel):
                 return (
                     f"TestModelModel(id={self.id}, name={self.name}, description={self.description}, mode={self.mode}, "
                     f"file_type={self.file_type}, model_type={self.model_type}, status={self.status}, size={self.size}, "
-                    f"serializer={self.serializer}, model_format={self.model_format}, datapath={self.datapath}, "
-                    f"filepath={self.filepath}, filename={self.filename},  "
+                    f"serializer={self.serializer}, model_format={self.model_format}"
+                    f"filename={self.filename},  "
                     f"created_at={self.created_at}, updated_at={self.updated_at})"
                 )
             case TestModelMode.API:
                 return (
                     f"TestModelModel(id={self.id}, name={self.name}, description={self.description}, mode={self.mode}, "
                     f"model_type={self.model_type}, status={self.status}, size={self.size}, "
-                    f"datapath={self.datapath}, model_api={self.model_api.decode('utf-8')}, "
+                    f"model_api={self.model_api.decode('utf-8') if self.model_api else ''}, "
                     f"created_at={self.created_at}, updated_at={self.updated_at})"
                 )
