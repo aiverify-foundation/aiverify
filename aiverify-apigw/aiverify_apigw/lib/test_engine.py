74be67a8a35c21c7f7242cc931f0006ec0146b06
Peck Yoke <leong_peck_yoke@imda.gov.sg>
2024-12-16 13:47:53 +0800

    lazy load aiverify test engine, and discover modules only on demand

--- aiverify-apigw/aiverify_apigw/lib/test_engine.py
+++ aiverify-apigw/aiverify_apigw/lib/test_engine.py
@@ -1,8 +1,7 @@
 from pathlib import Path
-from aiverify_test_engine.plugins.plugins_manager import PluginManager, IModel
-# from aiverify_test_engine.plugins.enums.model_plugin_type import ModelPluginType
-# from aiverify_test_engine.plugins.enums.serializer_plugin_type import SerializerPluginType
-from aiverify_test_engine.plugins.enums.plugin_type import PluginType
+import importlib.util
+import sys
+from typing import Any
 from .logging import logger
 
 
@@ -10,14 +9,35 @@ class TestEngineValidatorException(BaseException):
     pass
 
 
+def _lazy_import(module_name: str):
+    spec = importlib.util.find_spec(module_name)
+    if spec is None or spec.loader is None:
+        raise RuntimeError("Unable to load aiverify_test_engine module")
+    loader = importlib.util.LazyLoader(spec.loader)
+    spec.loader = loader
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    loader.exec_module(module)
+    return module
+
+
 class TestEngineValidator:
     core_modules_path = Path(__file__).parent.parent.parent.parent.joinpath("aiverify-test-engine/aiverify_test_engine/io").absolute()
+    engine_initialized: bool = False
+    plugins_manager: Any | None = None
 
     @classmethod
-    def init_engine(cls):
+    def init_engine(cls) -> Any:
+        if cls.plugins_manager is not None:
+            return cls.plugins_manager
         logger.debug(f"core_modules_path: {cls.core_modules_path}")
+        plugins_manager = _lazy_import('aiverify_test_engine.plugins.plugins_manager')
+        PluginManager = plugins_manager.PluginManager
         PluginManager.discover(cls.core_modules_path.as_posix())
         logger.info(f"Test Engine Plugins: {PluginManager.get_printable_plugins()}")
+        cls.engine_initialized = True
+        cls.plugins_manager = plugins_manager
+        return cls.plugins_manager
 
     @classmethod
     def validate_model(cls, model_path: Path) -> tuple[str, str]:
@@ -32,6 +52,10 @@ class TestEngineValidator:
         Returns:
             tuple[ModelPluginType, SerializerPluginType]: (model_format, serializer_type)
         """
+        from aiverify_test_engine.plugins.enums.plugin_type import PluginType
+        plugins_manager = cls.init_engine()
+        PluginManager = plugins_manager.PluginManager
+
         try:
             (model_instance, model_serializer, errmsg) = PluginManager.get_instance(
                 PluginType.MODEL, **{"filename": model_path.absolute().as_posix()}
@@ -39,7 +63,7 @@ class TestEngineValidator:
         except:
             raise TestEngineValidatorException(f"Invalid model format")
         logger.debug(f"validate_model. model_instance:{model_instance}, model_serializer: {model_serializer}, errmsg: {errmsg}")
-        if not isinstance(model_instance, IModel):
+        if not isinstance(model_instance, plugins_manager.IModel):
             raise TestEngineValidatorException(f"Invalid model instance returned: {type(model_instance)}")
         if model_instance and model_serializer:
             (is_success, error_messages) = model_instance.setup() 
@@ -53,5 +77,5 @@ class TestEngineValidator:
             raise TestEngineValidatorException(errmsg)
 
         
-# initialize the test engine
-TestEngineValidator.init_engine()
+# # initialize the test engine
+# TestEngineValidator.init_engine()
