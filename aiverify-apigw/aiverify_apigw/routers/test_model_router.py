ea70363a8e800c7566240d90ef3ca0b7ea4a0102
Peck Yoke <leong_peck_yoke@imda.gov.sg>
2024-12-17 21:50:17 +0800

    add model api

--- aiverify-apigw/aiverify_apigw/routers/test_model_router.py
+++ aiverify-apigw/aiverify_apigw/routers/test_model_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, UploadFile, Form, Depends, Response
+from fastapi import APIRouter, HTTPException, UploadFile, Form, Depends, Response, Body
 from typing import List, Annotated, Any, Literal
 from datetime import datetime, timezone
 from sqlalchemy.orm import Session
@@ -11,7 +11,7 @@ from ..lib.constants import ModelType, TestModelMode, TestModelFileType, TestMod
 from ..lib.file_utils import check_valid_filename, check_file_size
 from ..lib.filestore import save_test_model as fs_save_test_model, delete_test_model as fs_delete_test_model, get_test_model as fs_get_test_model
 from ..lib.database import get_db_session
-from ..schemas import TestModel, TestModelUpdate
+from ..schemas import TestModel, TestModelUpdate, TestModelAPIInput, load_examples
 from ..models import TestModelModel, TestResultModel
 from ..lib.test_engine import TestEngineValidator, TestEngineValidatorException
 
@@ -21,6 +21,8 @@ router = APIRouter(prefix="/test_models", tags=["test_models"])
 file_upload_form_example = [
     "classification"
 ]
+
+model_api_examples = load_examples("model_api_examples.json")
 
 
 @router.post("/upload", response_model=List[TestModel])
@@ -246,6 +248,40 @@ def upload_folder(
         raise HTTPException(status_code=500, detail="Internal server error")
 
 
+@router.post("/modelapi", response_model=TestModel)
+def create_test_model_api(input_data: Annotated[
+                            TestModelAPIInput,
+                            Body(
+                                openapi_examples=model_api_examples
+                            )
+                        ], 
+                        session: Session = Depends(get_db_session)):
+    """
+    Endpoint to create a new TestModelModel from TestModelAPIInput.
+    """
+    try:
+        now = datetime.now(timezone.utc)
+        test_model = TestModelModel(
+            name=input_data.name,
+            description=input_data.description,
+            mode=TestModelMode.API,
+            model_type=input_data.modelType,
+            model_api=input_data.modelAPI.model_dump_json().encode("utf-8"),
+            status=TestModelStatus.Valid,
+            created_at=now,
+            updated_at=now
+        )
+
+        session.add(test_model)
+        session.commit()
+        return TestModel.from_model(test_model)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        logger.error(f"Error creating test model: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+
 @router.get("/", response_model=List[TestModel])
 def list_test_models(session: Session = Depends(get_db_session)):
     """
@@ -328,6 +364,10 @@ def update_test_model(model_id: int, model_update: TestModelUpdate, session: Ses
             test_model.description = model_update.description
         if model_update.modelType:
             test_model.model_type = model_update.modelType
+        if model_update.modelAPI:
+            if test_model.mode != TestModelMode.API:
+                raise HTTPException(status_code=400, detail="Cannot update modelAPI field is mode is not API")
+            test_model.model_api = model_update.modelAPI.model_dump_json().encode("utf-8")
         test_model.updated_at = datetime.now(timezone.utc)
 
         session.commit()
