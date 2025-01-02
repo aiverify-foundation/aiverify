aa6683dd1165a77e3828f89e967406888a57ef44
Amin <aminurrashid_mohamed_lahir@imda.gov.sg>
2025-01-02 06:22:41 +0000

    templates initial layout

--- aiverify-portal/app/templates/page.tsx
+++ aiverify-portal/app/templates/page.tsx
@@ -1,7 +1,7 @@
 function TemplatesPage() {
   return (
     <main className="h-screen w-full px-6">
-      <h1 className="my-6 text-2xl font-bold tracking-wide">Templates Page</h1>
+      <h1 className="my-6 text-2xl font-bold tracking-wide">Select Template</h1>
     </main>
   );
 }
