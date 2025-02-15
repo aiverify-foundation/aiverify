3071587b9aaee4fc5b01b251f953ae4f96b0441a
imda-kwokwk <kwok_weng_kong@imda.gov.sg>
2025-02-16 00:31:59 +0800

    v2 pre-build-check

--- .ci/run-flake8.sh
+++ .ci/run-flake8.sh
@@ -9,7 +9,7 @@ echo "##########################################################################
 
 set +e
 flake8 --count  $source_dir > "${source_dir}-flake8-report.txt"
-cat flake8-report.txt
+cat "${source_dir}-flake8-report.txt"
 exit_code=$?
 
 if [ $exit_code -ne 0 ]; then
