b51b4ccf985a2844f141c43fe47f275f6975bd97
Amin <aminurrashid_mohamed_lahir@imda.gov.sg>
2025-02-25 22:04:12 +0000

    fix body overflow x

--- aiverify-portal/app/layout.tsx
+++ aiverify-portal/app/layout.tsx
@@ -9,12 +9,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-screen w-screen bg-gradient-to-b from-primary-950 to-secondary-700 pb-16 text-white antialiased">
-        <main>
-          {children}
-        </main>
+      <body className="min-h-screen w-full bg-gradient-to-b from-primary-950 to-secondary-700 pb-16 text-white antialiased">
+        <main>{children}</main>
       </body>
     </html>
   );
 }
-
