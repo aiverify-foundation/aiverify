a27458d98d8c3c35070f549a764b8b62f075defb
Chong Wan Fei <chong_wan_fei@imda.gov.sg>
2025-03-02 18:22:08 +0800

    format upload datasets page

--- aiverify-portal/app/datasets/upload/page.tsx
+++ aiverify-portal/app/datasets/upload/page.tsx
@@ -2,14 +2,9 @@ import { DatasetUploader } from './components/datasetUploader';
 
 function UploadResultsPage() {
   return (
-    <main className="h-screen w-full px-6">
-      <div className="flex items-center justify-between">
-        <h1 className="my-6 text-2xl font-bold tracking-wide">
-          Add New Datasets
-        </h1>
-      </div>
-      <DatasetUploader className="mt-12" />
-    </main>
+    <div className="p-6">
+      <DatasetUploader />
+    </div>
   );
 }
 
