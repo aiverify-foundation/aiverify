d64a195bd7d2baf55f77e5f1a0bb30e6b3544a5f
imda-kwokwk <kwok_weng_kong@imda.gov.sg>
2025-04-09 21:20:08 +0800

    build and push docker image to ghcr

--- .ci/free_disk_space.sh
+++ .ci/free_disk_space.sh
@@ -49,7 +49,8 @@ sudo apt-get remove -y '^llvm-.*'
 sudo apt-get remove -y 'php.*'
 sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
 sudo apt-get remove -y 'temurin-*'
-sudo apt-get remove -y microsoft-edge-stable google-chrome-stable google-cloud-cli-anthoscli mysql-server-core-8.0 postgresql-16
+sudo apt-get remove -y google-cloud-cli microsoft-edge-stable google-chrome-stable google-cloud-cli-anthoscli
+sudo apt-get remove -y mysql-server-core-8.0 postgresql-16
 sudo apt-get autoremove -y
 sudo apt-get clean
 #df -h
