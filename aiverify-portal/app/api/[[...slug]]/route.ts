42f7a1bdb0a807d8032078db5ff2ef23b3ae9fa4
Leong Peck Yoke <leong_peck_yoke@imda.gov.sg>
2025-05-20 21:59:11 +0800

    catch fetch error and handle redirect

--- aiverify-portal/app/api/[[...slug]]/route.ts
+++ aiverify-portal/app/api/[[...slug]]/route.ts
@@ -9,27 +9,59 @@ async function _backendFetch(
   method: string,
   slug?: string[] | undefined
 ) {
+  // console.log('request:', request);
   const path = slug ? slug.join('/') : '';
   const url = new URL(request.url);
+  // console.log('path:', path);
+  // console.log('url:', url);
   const searchParam = new URLSearchParams(url.search);
-  const backendUrl = new URL(
-    path + (searchParam.size > 0 ? '?' + searchParam.toString() : ''),
-    APIGW_HOST
-  );
-  console.log('backendUrl:', backendUrl);
+  let mypath =
+    path + (searchParam.size > 0 ? '?' + searchParam.toString() : '');
+  if (request.url.endsWith('/') && !mypath.endsWith('/')) {
+    mypath += '/';
+  }
+  const backendUrl = new URL(mypath, APIGW_HOST);
+  // console.log('backendUrl:', backendUrl);
 
-  const response = await fetch(backendUrl, {
-    method: method,
-    headers: request.headers,
-    body: request.body,
-    // @ts-ignore
-    duplex: 'half',
-  });
+  try {
+    const body = ['GET', 'HEAD'].includes(request.method)
+      ? null
+      : await request.blob();
+    // console.log('body:', body);
+    let response = await fetch(backendUrl, {
+      method: method,
+      headers: request.headers,
+      body,
+      // @ts-ignore
+      // duplex: 'half',
+      redirect: 'manual',
+    });
+    // console.log('response:', response);
 
-  return new NextResponse(response.body, {
-    status: response.status,
-    headers: response.headers,
-  });
+    if (response.status === 307 && response.headers.has('location')) {
+      response = await fetch(
+        response.headers.get('location') || backendUrl + '/',
+        {
+          method: method,
+          headers: request.headers,
+          body,
+        }
+      );
+      // console.log('response 2:', response);
+    }
+
+    return new NextResponse(response.body, {
+      status: response.status,
+      headers: response.headers,
+    });
+  } catch (e: unknown) {
+    console.log('apigw fetch error: ', e);
+    if (e instanceof Error) {
+      return new NextResponse(e.message, { status: 500 });
+    } else {
+      return new NextResponse('Unknown error', { status: 500 });
+    }
+  }
 }
 
 export async function GET(
