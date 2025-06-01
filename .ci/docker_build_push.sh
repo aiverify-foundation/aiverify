6dfc33094fa6014f9d49ef6d2980f4768bd5077b
Benedict Lee <118954493+imda-benedictlee@users.noreply.github.com>
2025-06-01 15:42:55 +0800

    Add A Conditional Block To Build AI Verify Python Base Image If Push Value is False

--- .ci/docker_build_push.sh
+++ .ci/docker_build_push.sh
@@ -13,42 +13,42 @@ GITHUB_USERNAME=$3
 DOCKERFILE_DIR=$4
 TARGET=${5:-}
 TAG_SUFFIX=${6:-}
+PUSH=${7:-}
 
-# Login to GitHub Container Registry
-echo $GITHUB_TOKEN | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
-
-# echo "Create a new build instance..."
-
-# Create a new builder instance
-docker buildx create --name mybuilder --use
-
-# Inspect the builder instance
-docker buildx inspect --bootstrap
-
-echo "Build and push image name=$IMAGE_NAME tag=$TAG target=$TARGET tag_suffix=$TAG_SUFFIX..."
-
-# Build and push the image for both amd64 and arm64 platforms
-
-if [ -n "$TARGET" ]; then
+if [[ "$PUSH" = false ]]; then
     # Build aiverify-python-base
-    docker buildx build --platform linux/amd64,linux/arm64 -t aiverify-python-base -f aiverify-test-engine/Dockerfile .
-    docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG-$TAG_SUFFIX -f $DOCKERFILE_DIR/Dockerfile --provenance=false --sbom=false --target $TARGET --push .
+    docker buildx build --platform linux/amd64,linux/arm64 -t $IMAGE_NAME -f $DOCKERFILE_DIR/Dockerfile .
 else
-    # Build aiverify-python-base
-    docker buildx build --platform linux/amd64,linux/arm64 -t aiverify-python-base -f aiverify-test-engine/Dockerfile .
-    docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG -f $DOCKERFILE_DIR/Dockerfile --provenance=false --sbom=false --push .
-fi
+    # Login to GitHub Container Registry
+    echo $GITHUB_TOKEN | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
 
-echo "Create and push manifests..."
+    # echo "Create a new build instance..."
 
-# Create and push the manifest for specified tag and the latest tag
-if [ -n "$TARGET" ]; then
-    docker buildx imagetools create -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG-$TAG_SUFFIX ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG-$TAG_SUFFIX
-    docker buildx imagetools create -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:latest-$TAG_SUFFIX ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG-$TAG_SUFFIX
-else
-    docker buildx imagetools create -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG
-    docker buildx imagetools create -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:latest ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG
-fi
+    # Create a new builder instance
+    docker buildx create --name mybuilder --use
+
+    # Inspect the builder instance
+    docker buildx inspect --bootstrap
+
+    echo "Build and push image name=$IMAGE_NAME tag=$TAG target=$TARGET tag_suffix=$TAG_SUFFIX..."
+
+    # Build and push the image for both amd64 and arm64 platforms
+    if [ -n "$TARGET" ]; then
+        docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG-$TAG_SUFFIX -f $DOCKERFILE_DIR/Dockerfile --provenance=false --sbom=false --target $TARGET --push .
+    else
+        docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG -f $DOCKERFILE_DIR/Dockerfile --provenance=false --sbom=false --push .
+    fi
+
+    echo "Create and push manifests..."
+
+    # Create and push the manifest for specified tag and the latest tag
+    if [ -n "$TARGET" ]; then
+        docker buildx imagetools create -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG-$TAG_SUFFIX ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG-$TAG_SUFFIX
+        docker buildx imagetools create -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:latest-$TAG_SUFFIX ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG-$TAG_SUFFIX
+    else
+        docker buildx imagetools create -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG
+        docker buildx imagetools create -t ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:latest ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$TAG
+    fi
 
 # Clean up build cache
 yes | docker builder prune --all
