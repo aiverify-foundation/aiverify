e33526a88c8acca51fcb04f4b8dc59867513741d
Leong Peck Yoke <leong_peck_yoke@imda.gov.sg>
2025-06-09 13:06:26 +0800

    add unit tests for project_router module

--- aiverify-apigw/tests/conftest.py
+++ aiverify-apigw/tests/conftest.py
@@ -158,6 +158,19 @@ def mock_project_template(db_session):
 
 
 @pytest.fixture(scope="function")
+def mock_projects(db_session, mock_project_template):
+    from .mocks.mock_data_project import create_mock_project
+    from aiverify_apigw.models import ProjectModel
+    project1 = create_mock_project(db_session, mock_project_template)
+    project2 = create_mock_project(db_session)
+    projects = [project1, project2]
+    db_session.commit()
+    yield projects
+    db_session.query(ProjectModel).delete()
+    db_session.commit()
+
+
+@pytest.fixture(scope="function")
 def mock_input_block_data(db_session, mock_plugins):
     from .mocks.mock_input_block_data import create_mock_input_block_data
     from aiverify_apigw.models import InputBlockDataModel
--- aiverify-apigw/tests/mocks/mock_data_project.py
+++ aiverify-apigw/tests/mocks/mock_data_project.py
@@ -1,6 +1,6 @@
 from faker import Faker
 faker = Faker()
-from aiverify_apigw.models import ProjectTemplateModel
+from aiverify_apigw.models import ProjectTemplateModel, ProjectModel
 from .mock_project_meta import create_mock_project_template_data_meta
 
 
@@ -17,3 +17,20 @@ def create_mock_project_templates(session, template_id: str | None = None):
     session.add(project_template)
     session.flush()
     return project_template
+
+
+def create_mock_project(session, template: ProjectTemplateModel | None=None):
+    project = ProjectModel(
+        name=faker.name(),
+        description=faker.text(max_nb_chars=256),
+        data=b'{}',  # Assuming empty JSON data for mock
+        template= template,
+        report_title=faker.sentence(),
+        company=faker.company(),
+        created_at=faker.date_time_this_year(),
+        updated_at=faker.date_time_this_year(),
+        user_id=None  # Assuming no user is associated in the mock
+    )
+    session.add(project)
+    session.flush()
+    return project
