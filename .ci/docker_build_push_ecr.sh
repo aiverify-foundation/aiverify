b8cf3fd5f4abafffd3544caba3fbc805e401bea4
Benedict Lee <benedict_lee@imda.gov.sg>
2025-08-21 14:00:42 +0800

    Change DOCKERFILE_DIR to DOCKERFILE_PATH

--- .ci/docker_build_push_ecr.sh
+++ .ci/docker_build_push_ecr.sh
@@ -50,7 +50,7 @@ fi
 IMAGE_NAME=$1
 TAG=$2
 GITHUB_USERNAME=$3
-DOCKERFILE_DIR=$4
+DOCKERFILE_PATH=$4
 TARGET=${5:-}
 TAG_SUFFIX=${6:-}
 
@@ -72,9 +72,9 @@ echo "Build and push image name=$IMAGE_NAME tag=$TAG target=$TARGET tag_suffix=$
 
 # Build and push the image for both amd64 and arm64 platforms
 if [ -n "$TARGET" ]; then
-    docker buildx build --platform linux/amd64,linux/arm64 -t $ECR_IMAGE_URI:$TAG-$TAG_SUFFIX -f $DOCKERFILE_DIR/Dockerfile --provenance=false --sbom=false --target $TARGET --push .
+    docker buildx build --platform linux/amd64,linux/arm64 -t $ECR_IMAGE_URI:$TAG-$TAG_SUFFIX -f $DOCKERFILE_PATH --provenance=false --sbom=false --target $TARGET --push .
 else
-    docker buildx build --platform linux/amd64,linux/arm64 -t $ECR_IMAGE_URI:$TAG -f $DOCKERFILE_DIR/Dockerfile --provenance=false --sbom=false --push .
+    docker buildx build --platform linux/amd64,linux/arm64 -t $ECR_IMAGE_URI:$TAG -f $DOCKERFILE_PATH --provenance=false --sbom=false --push .
 fi
 
 echo "Create and push manifests..."
