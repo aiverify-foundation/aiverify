17c5b6ccbe6f9c99c04ccd68cea5083a664cbcb9
Benedict Lee <benedict_lee@imda.gov.sg>
2025-08-21 17:23:17 +0800

    Update Node Name

--- .ci/docker_build_push.sh
+++ .ci/docker_build_push.sh
@@ -23,7 +23,7 @@ echo $GITHUB_TOKEN | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
 # echo "Create a new build instance..."
 
 # Create a new builder instance
-docker buildx create --name imagebuilder --use
+docker buildx create --name imagebuilder --append $IMAGE_NAME
 
 # Inspect the builder instance
 docker buildx inspect --bootstrap
